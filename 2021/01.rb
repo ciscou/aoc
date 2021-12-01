7f702fe09e96e27f269064d56cfcb4b26135dee0
ciscou <francismpp@gmail.com>
2021-12-01 14:36:07 +0100

    refactoring

--- 2021/01.rb
+++ 2021/01.rb
@@ -1,8 +1,8 @@
-input = File.read(__FILE__.sub('.rb', '.txt')).lines.map(&:chomp)
+INPUT = File.read(__FILE__.sub('.rb', '.txt')).lines.map(&:chomp)
 
 def increases(measurements)
   measurements.each_cons(2).count { |a, b| b > a }
 end
 
-puts increases(input.map(&:to_i))
-puts increases(input.map(&:to_i).each_cons(3).map(&:sum))
+puts increases(INPUT.map(&:to_i))
+puts increases(INPUT.map(&:to_i).each_cons(3).map(&:sum))
