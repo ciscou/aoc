4f1034874026936f40ac8b4e3d763614a923b6d7
ciscou <francismpp@gmail.com>
2021-12-01 14:23:02 +0100

    2020-23 part2

--- 2020/23.rb
+++ 2020/23.rb
@@ -1,54 +1,58 @@
 input = 925176834
 
-cups = input.to_s.chars.map(&:to_i).map { |label| { label: label } }
-cups.each_cons(2) { |a, b| a[:next] = b }
-cups.each_cons(2) { |a, b| b[:prev] = a }
-cups.first[:prev] = cups.last
-cups.last[:next] = cups.first
-current = cups.first
+def perform_moves(input, moves_count, cups_count)
+  cups = input.to_s.chars.map(&:to_i).map { |label| { label: label } }
+  ((cups.length + 1)..cups_count).each { |label| cups << { label: label } }
+  cups.each_cons(2) { |a, b| a[:next] = b }
+  cups.each_cons(2) { |a, b| b[:prev] = a }
+  cups.first[:prev] = cups.last
+  cups.last[:next] = cups.first
+  current = cups.first
 
-cups_by_label = cups.group_by { |cup| cup[:label] }
+  cups_by_label = cups.group_by { |cup| cup[:label] }
 
-100.times do
-  kks = []
-  kk = current
-  9.times do
-    kks << kk
-    kk = kk[:next]
+  moves_count.times do
+    picks = []
+    picks << current[:next]
+    picks << picks.last[:next]
+    picks << picks.last[:next]
+
+    current[:next] = picks.last[:next]
+    picks.last[:next][:prev] = current
+
+    destination = current[:label] - 1
+    destination = cups_count if destination < 1
+    while picks.map { |pick| pick[:label] }.include?(destination)
+      destination -= 1
+      destination = cups_count if destination < 1
+    end
+    destination = cups_by_label[destination].first
+
+    destination[:next][:prev] = picks.last
+    picks.last[:next] = destination[:next]
+    destination[:next] = picks.first
+    picks.first[:prev] = destination
+
+    current = current[:next]
   end
-  puts kks.map { |cup| cup[:label] }.join(", ")
 
-  picks = []
-  picks << current[:next]
-  picks << picks.last[:next]
-  picks << picks.last[:next]
-  puts picks.map { |cup| cup[:label] }.join(", ")
+  cups_by_label
+end
 
-  current[:next] = picks.last[:next]
-  picks.last[:next][:prev] = current
-
-  destination = current[:label] - 1
-  destination = 9 if destination < 1
-  while picks.map { |pick| pick[:label] }.include?(destination)
-    destination -= 1
-    destination = 9 if destination < 1
-  end
-  puts "@@@#{destination.inspect}@@@"
-  destination = cups_by_label[destination].first
-  puts destination[:label]
-
-  destination[:next][:prev] = picks.last
-  picks.last[:next] = destination[:next]
-  destination[:next] = picks.first
-  picks.first[:prev] = destination
-
+cups_by_label_1 = perform_moves(input, 100, 9)
+part1 = []
+current = cups_by_label_1[1].first
+9.times do
+  part1 << current
   current = current[:next]
 end
+puts part1[1..-1].map { |cup| cup[:label] }.join("")
 
-kks = []
-kk = cups_by_label[1].first
-9.times do
-  kks << kk
-  kk = kk[:next]
+cups_by_label_2 = perform_moves(input, 10_000_000, 1_000_000)
+part2 = []
+current = cups_by_label_2[1].first
+3.times do
+  part2 << current
+  current = current[:next]
 end
-puts kks.map { |cup| cup[:label] }.join("")
+puts part2[1][:label] * part2[2][:label]
