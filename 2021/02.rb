cf3f9e9f870ed509c8abfebe6a9ecad8b87a1f32
ciscou <francismpp@gmail.com>
2021-12-02 08:57:53 +0100

    2021 02 part2

--- 2021/02.rb
+++ 2021/02.rb
@@ -1,13 +1,39 @@
 INPUT = File.read(__FILE__.sub('.rb', '.txt')).lines.map(&:chomp)
 
+def part1(commands)
+  commands_by_direction = commands.group_by(&:first)
+
+  final_depth = commands_by_direction["down"].map(&:last).sum - commands_by_direction["up"].map(&:last).sum
+  final_horizontal_position = commands_by_direction["forward"].map(&:last).sum
+
+  [final_depth, final_horizontal_position]
+end
+
+def part2(commands)
+  aim = 0
+  depth = 0
+  horizontal_position = 0
+
+  commands.each do |command|
+    direction, count = command
+    case direction
+    when "down"
+      aim += count
+    when "up"
+      aim -= count
+    when "forward"
+      horizontal_position += count
+      depth += aim * count
+    end
+  end
+
+  [depth, horizontal_position]
+end
+
 commands = INPUT.map do |command|
   direction, count = command.split
   [direction, count.to_i]
 end
 
-commands_by_direction = commands.group_by(&:first)
-
-final_depth = commands_by_direction["down"].map(&:last).sum - commands_by_direction["up"].map(&:last).sum
-final_horizontal_position = commands_by_direction["forward"].map(&:last).sum
-
-puts final_depth * final_horizontal_position
+puts part1(commands).inject(:*)
+puts part2(commands).inject(:*)
