73e66ddb5c79624c9f7ed847df0f7772a0fbdb28
ciscou <francismpp@gmail.com>
2021-12-03 22:17:59 +0100

    refactor and complete solution

--- 2019/18.rb
+++ 2019/18.rb
@@ -1,5 +1,8 @@
 INPUT = File.read(__FILE__.sub('.rb', '.txt')).lines.map(&:chomp)
 
+$max_moves = 0
+$max_collected_keys = 0
+
 class Maze
   def initialize
     @cells = INPUT.map(&:chars)
@@ -17,30 +20,65 @@ class Maze
     @collected_keys = []
   end
 
-  def move(drow, dcol)
-    @row += drow
-    @col += dcol
+  def setup_part_1!
+    @row = [@row]
+    @col = [@col]
+  end
+
+  def setup_part_2!
+    @cells[@row][@col] = "#"
+
+    @cells[@row+1][@col] = "#"
+    @cells[@row-1][@col] = "#"
+    @cells[@row][@col+1] = "#"
+    @cells[@row][@col-1] = "#"
+
+    @cells[@row+1][@col+1] = "@"
+    @cells[@row+1][@col-1] = "@"
+    @cells[@row-1][@col+1] = "@"
+    @cells[@row-1][@col-1] = "@"
+
+    @row = []
+    @col = []
+
+    @cells.length.times do |row|
+      @cells.first.length.times do |col|
+        cell = @cells[row][col]
+        @keys << cell if ("a".."z").include?(cell)
+        if cell == "@"
+          @row << row
+          @col << col
+        end
+      end
+    end
+  end
+
+  def move(index, drow, dcol)
+    @row[index] += drow
+    @col[index] += dcol
   end
 
   def valid_position?
-    return false if @row < 0
-    return false if @col < 0
-    return false if @row >= @cells.length
-    return false if @col >= @cells.first.length
-    return false if cell == "#"
-    return false if ("A".."Z").include?(cell) && !@collected_keys.include?(cell.downcase)
+    @row.length.times do |index|
+      return false if @row[index] < 0
+      return false if @col[index] < 0
+      return false if @row[index] >= @cells.length
+      return false if @col[index] >= @cells.first.length
+      return false if cell(index) == "#"
+      return false if ("A".."Z").include?(cell(index)) && !@collected_keys.include?(cell(index).downcase)
+    end
 
     true
   end
 
-  def cell
-    @cells[@row][@col]
+  def cell(index)
+    @cells[@row[index]][@col[index]]
   end
 
-  def collect_key!
-    if ("a".."z").include?(cell) && !@collected_keys.include?(cell)
-      @collected_keys << cell
-      cell
+  def collect_key!(index)
+    if ("a".."z").include?(cell(index)) && !@collected_keys.include?(cell(index))
+      @collected_keys << cell(index)
+      cell(index)
     end
   end
 
@@ -52,14 +90,37 @@ class Maze
     visited = {}
     queue = []
 
-    visited[[@row, @col, @collected_keys.sort.join("")]] = true
-    queue.push([@row, @col, @collected_keys.join(""), 0])
+    visited[[@row.dup, @col.dup, @collected_keys.sort.join("")]] = true
+    queue.push([@row.dup, @col.dup, @collected_keys.join(""), 0])
 
     until queue.empty?
       node = queue.shift
 
       @row, @col, keys, moves = node
       @collected_keys = keys.split("")
+
+      if moves > $max_moves
+        $max_moves = moves
+        puts "max moves: #{moves}"
+      end
+
+      if keys.size > $max_collected_keys
+        $max_collected_keys = keys.length
+        puts "max collected keys: #{keys.length}"
+      end
+
+      if keys.length == 0
+        if moves > 50
+          next
+        end
+      elsif moves / keys.length > 50
+        next
+      end
+
+      # if moves > 50
+      #   puts "next"
+      #   next
+      # end
 
       if all_keys_collected?
         puts "solved in #{moves} moves"
@@ -68,20 +129,31 @@ class Maze
       end
 
       available_moves = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-      available_moves.each do |drow, dcol|
-        move(drow, dcol)
-        if valid_position? && !visited[[@row, @col, @collected_keys.sort.join("")]]
-          @collected_keys_was = @collected_keys.join("").split("")
-          collect_key!
-          visited[[@row, @col, @collected_keys.sort.join("")]] = true
-          queue.push([@row, @col, @collected_keys.join(""), moves + 1])
-          @collected_keys = @collected_keys_was
+      @row.length.times do |index|
+        available_moves.each do |drow, dcol|
+          move(index, drow, dcol)
+          if valid_position? && !visited[[@row.dup, @col.dup, @collected_keys.sort.join("")]]
+            @collected_keys_was = @collected_keys.join("").split("")
+            collect_key!(index)
+            visited[[@row.dup, @col.dup, @collected_keys.sort.join("")]] = true
+            queue.push([@row.dup, @col.dup, @collected_keys.join(""), moves + 1])
+            @collected_keys = @collected_keys_was
+          end
+          move(index, -drow, -dcol)
         end
-        move(-drow, -dcol)
       end
     end
   end
 end
 
-maze = Maze.new
-maze.solve
+# start = Time.now
+# maze1 = Maze.new
+# maze1.setup_part_1!
+# maze1.solve
+# puts "solved in #{Time.now - start} seconds"
+
+start = Time.now
+maze2 = Maze.new
+maze2.setup_part_2!
+maze2.solve
+puts "solved in #{Time.now - start} seconds"
--- 2020/20.rb
+++ 2020/20.rb
@@ -5,64 +5,37 @@ class Tile
     @id = id
     @cells = cells
     @original_cells = cells
-    @flips = 0
-    @rotations = 0
     @size = cells.length
   end
 
   def reset!
-    @flips = 0
-    @rotations = 0
     @cells = @original_cells
   end
 
   def flip!
-    @flips += 1
-    @flips %= 2
     @cells = @cells.map(&:reverse)
   end
 
   def rotate!
-    @rotations += 1
-    @rotations %= 4
     @cells = @cells.transpose.map(&:reverse)
-  end
-
-  def encode_borders
-    @borders = {}
-
-    2.times do |flips|
-      4.times do |rotations|
-        reset!
-        flips.times { flip! }
-        rotations.times { rotate! }
-
-        @borders[flips] ||= {}
-        @borders[flips][rotations] ||= {}
-        @borders[flips][rotations][:top] = encode_border(@cells.first)
-        @borders[flips][rotations][:bottom] = encode_border(@cells.last)
-        @borders[flips][rotations][:left] = encode_border(@size.times.map { |i| @cells[i][0] })
-        @borders[flips][rotations][:right] = encode_border(@size.times.map { |i| @cells[i][@size-1] })
-      end
-    end
   end
 
   attr_reader :id
 
   def top
-    @borders[@flips][@rotations][:top]
+    encode_border(@cells.first)
   end
 
   def bottom
-    @borders[@flips][@rotations][:bottom]
+    encode_border(@cells.last)
   end
 
   def left
-    @borders[@flips][@rotations][:left]
+    encode_border(@size.times.map { |i| @cells[i][0] })
   end
 
   def right
-    @borders[@flips][@rotations][:right]
+    encode_border(@size.times.map { |i| @cells[i][@size-1] })
   end
 
   def can_go_right_to?(other_tile)
@@ -138,7 +111,7 @@ class Tile
   end
 end
 
-def valid_solution?(solution)
+def valid_partial_solution?(solution)
   solution.each_with_index do |tile, index|
     next if tile.nil?
 
@@ -155,8 +128,6 @@ tiles = INPUT.each_slice(12).map do |lines|
   Tile.new(id: lines[0].split(" ").last.to_i, cells: lines[1, 10].map(&:chars))
 end
 
-tiles.each(&:encode_borders)
-
 SQUARE_SIZE = Math.sqrt(tiles.size).round
 
 queue = []
@@ -169,62 +140,43 @@ tiles.length.times do |idx|
   end
 end
 
-# solution = nil
-# max_tiles_specs_size = 0
+solution = nil
 
-# until queue.empty?
-#   tiles_specs = queue.pop
+until queue.empty?
+  tiles_specs = queue.pop
 
-#   if tiles_specs.size > max_tiles_specs_size
-#     max_tiles_specs_size = tiles_specs.size
-#     puts max_tiles_specs_size
-#   end
+  available = tiles.length.times.to_a
 
-#   available = tiles.length.times.to_a
+  arrangement = tiles_specs.map do |idx, flip, rotate|
+    available.delete(idx)
 
-#   arrangement = tiles_specs.map do |idx, flip, rotate|
-#     available.delete(idx)
+    tile = tiles[idx]
+    tile.reset!
+    flip.times { tile.flip! }
+    rotate.times { tile.rotate! }
+    tile
+  end
 
-#     tile = tiles[idx]
-#     tile.reset!
-#     flip.times { tile.flip! }
-#     rotate.times { tile.rotate! }
-#     tile
-#   end
+  next unless valid_partial_solution?(arrangement)
 
-#   next unless valid_solution?(arrangement)
+  available.each do |idx|
+    2.times do |flip|
+      4.times do |rotate|
+        queue << tiles_specs + [[idx, flip, rotate]]
+      end
+    end
+  end
 
-#   available.each do |idx|
-#     2.times do |flip|
-#       4.times do |rotate|
-#         queue << tiles_specs + [[idx, flip, rotate]]
-#       end
-#     end
-#   end
+  if arrangement.length == tiles.length
+    solution = arrangement
+    break
+  end
+end
 
-#   if arrangement.length == tiles.length
-#     p tiles_specs
-#     solution = arrangement
-#     break
-#   end
-# end
-
-# if solution.nil?
-#   puts "no solution found!!!"
-# else
-#   puts [0, SQUARE_SIZE - 1, solution.length - SQUARE_SIZE, solution.length - 1].map { |idx| solution[idx].id }.inject(:*)
-# end
-
-# exit
-
-tiles_specs = [[97, 1, 2], [126, 0, 1], [118, 1, 1], [10, 0, 3], [136, 0, 0], [134, 1, 1], [61, 0, 3], [93, 1, 3], [54, 1, 3], [49, 0, 0], [83, 1, 3], [90, 0, 2], [57, 1, 2], [5, 0, 3], [143, 0, 3], [45, 1, 3], [13, 0, 0], [65, 0, 2], [44, 1, 2], [78, 0, 3], [30, 1, 1], [1, 1, 2], [14, 1, 1], [124, 0, 0], [129, 0, 3], [4, 0, 0], [11, 1, 1], [64, 0, 1], [81, 0, 3], [7, 0, 2], [99, 0, 1], [75, 1, 1], [102, 0, 3], [123, 0, 3], [138, 0, 2], [82, 0, 1], [18, 1, 3], [77, 0, 0], [108, 0, 2], [91, 1, 1], [58, 1, 3], [32, 0, 3], [23, 1, 1], [135, 1, 2], [121, 0, 1], [116, 1, 2], [122, 0, 3], [42, 1, 1], [12, 0, 2], [125, 1, 3], [110, 1, 3], [127, 1, 1], [63, 0, 2], [137, 0, 0], [37, 0, 2], [105, 0, 3], [100, 0, 2], [120, 0, 2], [115, 1, 1], [114, 1, 3], [19, 1, 2], [43, 1, 2], [72, 1, 3], [84, 0, 0], [76, 0, 3], [70, 0, 0], [106, 1, 1], [56, 0, 2], [111, 1, 2], [139, 1, 2], [119, 1, 2], [52, 0, 2], [38, 0, 0], [130, 1, 1], [128, 1, 1], [107, 0, 1], [94, 0, 3], [109, 1, 2], [3, 0, 3], [51, 1, 1], [28, 0, 0], [92, 0, 1], [66, 0, 3], [133, 0, 1], [142, 0, 1], [113, 1, 3], [140, 0, 0], [141, 1, 2], [6, 0, 2], [50, 0, 3], [68, 0, 0], [132, 0, 0], [98, 1, 3], [27, 0, 1], [36, 0, 3], [74, 0, 2], [62, 0, 2], [2, 1, 2], [86, 1, 3], [96, 1, 1], [53, 1, 1], [104, 1, 1], [22, 0, 3], [29, 0, 2], [112, 0, 2], [73, 0, 1], [40, 1, 2], [20, 0, 1], [33, 1, 2], [8, 1, 3], [41, 0, 3], [59, 0, 2], [131, 1, 2], [21, 1, 3], [15, 0, 0], [35, 0, 0], [103, 0, 3], [79, 1, 1], [67, 0, 3], [85, 1, 2], [39, 1, 2], [25, 0, 1], [26, 0, 2], [89, 0, 2], [69, 1, 1], [60, 1, 2], [48, 1, 3], [80, 0, 3], [17, 0, 1], [71, 0, 1], [9, 0, 3], [101, 1, 1], [34, 1, 2], [95, 1, 2], [87, 0, 3], [24, 1, 3], [117, 0, 3], [0, 0, 3], [47, 1, 2], [55, 0, 2], [46, 0, 2], [31, 0, 1], [88, 1, 2], [16, 1, 1]]
-
-solution = tiles_specs.map do |idx, flip, rotate|
-  tile = tiles[idx]
-  tile.reset!
-  flip.times { tile.flip! }
-  rotate.times { tile.rotate! }
-  tile
+if solution.nil?
+  puts "no solution found!!!"
+else
+  puts [0, SQUARE_SIZE - 1, solution.length - SQUARE_SIZE, solution.length - 1].map { |idx| solution[idx].id }.inject(:*)
 end
 
 sea_monsters_image = []
