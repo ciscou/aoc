54e46b458e3615725a1d70a2015168ae301e9c6b
ciscou <francismpp@gmail.com>
2021-12-03 13:43:41 +0100

    2020 21 part2

--- 2020/21.rb
+++ 2020/21.rb
@@ -1,12 +1,14 @@
 INPUT = File.read(__FILE__.sub('.rb', '.txt')).lines.map(&:chomp)
 
-def part1
+def helper
   foods = INPUT.map do |line|
     ingredients, allergens = line.split(" (")
     { ingredients: ingredients.split(" "), allergens: allergens[9..-2].split(", ") }
   end
 
   allergens = foods.flat_map { |f| f[:allergens] }.sort.uniq
+
+  ingredients_and_allergens = []
 
   loop do
     found = []
@@ -26,13 +28,24 @@ def part1
         food[:ingredients].delete(ingredient)
       end
 
+      ingredients_and_allergens << [ingredient, allergen]
+
       allergens.delete(allergen)
     end
 
     break if found.empty?
   end
 
-  foods
+  [foods, ingredients_and_allergens]
 end
 
-puts part1.select { |f| f[:allergens].empty? }.sum { |f| f[:ingredients].length }
+def part1
+  helper.first.select { |f| f[:allergens].empty? }.sum { |f| f[:ingredients].length }
+end
+
+def part2
+  helper.last.sort_by(&:last).map(&:first).join(",")
+end
+
+puts part1
+puts part2
