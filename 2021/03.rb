11061c0fe2dedd007c70475a9f3b042374dc3f7d
ciscou <francismpp@gmail.com>
2021-12-03 10:25:57 +0100

    2021 03 part2

--- 2021/03.rb
+++ 2021/03.rb
@@ -16,4 +16,47 @@ def part1
   [γ, ε].map { |s| s.to_i(2) }
 end
 
+def find_o2(numbers, offset)
+  if numbers.length == 1
+    return numbers.first
+  end
+
+  counts = numbers.each_with_object({ "0" => [], "1" => [] }) do |e, a|
+    e.chars.each_with_index do |b, i|
+      a["0"][i] ||= 0
+      a["1"][i] ||= 0
+      a[b][i] += 1
+    end
+  end
+
+  most_common = counts["0"][offset] > counts["1"][offset] ? "0" : "1"
+  most_common = "1" if counts["0"][offset] == counts["1"][offset]
+
+  find_o2(numbers.select { |n| n[offset] == most_common }, offset + 1)
+end
+
+def find_co2(numbers, offset)
+  if numbers.length == 1
+    return numbers.first
+  end
+
+  counts = numbers.each_with_object({ "0" => [], "1" => [] }) do |e, a|
+    e.chars.each_with_index do |b, i|
+      a["0"][i] ||= 0
+      a["1"][i] ||= 0
+      a[b][i] += 1
+    end
+  end
+
+  least_common = counts["0"][offset] < counts["1"][offset] ? "0" : "1"
+  least_common = "0" if counts["0"][offset] == counts["1"][offset]
+
+  find_co2(numbers.select { |n| n[offset] == least_common }, offset + 1)
+end
+
+def part2
+  [find_o2(INPUT, 0), find_co2(INPUT, 0)].map { |s| s.to_i(2) }
+end
+
 puts part1.inject(:*)
+puts part2.inject(:*)
