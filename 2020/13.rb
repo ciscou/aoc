f412e0ea6bf563282f27ed63e7d1ddf246eaca8d
ciscou <francismpp@gmail.com>
2021-12-05 19:21:56 +0100

    2020 13 part2

--- 2020/13.rb
+++ 2020/13.rb
@@ -27,9 +27,47 @@ def part1(input)
 end
 
 def part2(input)
-  problem = input.lines.last.split(",").map.with_index { |bus_id, offset| [bus_id == "x" ? nil : bus_id.to_i, offset] }.reject { |bus_id, offset| bus_id.nil? }.each do |a, b|
-    puts "#{a.to_s.rjust(3, " ")} * n = t + #{b}"
+  bus_ids_and_offsets = input.lines.last.split(",").map.with_index { |bus_id, offset| [bus_id == "x" ? nil : bus_id.to_i, offset] }.reject { |bus_id, offset| bus_id.nil? }
+
+  crt = crt(bus_ids_and_offsets.map(&:last), bus_ids_and_offsets.map(&:first))
+
+  puts crt.last - crt.first
+end
+
+# shamelessly stolen from https://linuxtut.com/en/f652d4941298a9842dfc/
+def ext_gcd(a, b, values)
+  d = a
+
+  if b.zero?
+    values[:x] = 1
+    values[:y] = 0
+  else
+    d = ext_gcd(b, a % b, values)
+
+    x = values[:x]
+    y = values[:y]
+    values[:x] = y
+    values[:y] = x - (a / b) * y
   end
+
+  d
+end
+
+def crt(b, m)
+  r = 0
+  mM = 1
+  b.zip(m) do |bi, mi|
+    val = {}
+    d = ext_gcd(mM, mi, val)
+    p = val[:x]
+    q = val[:y]
+    return [0, -1] if (bi - r) % d != 0
+    tmp = (bi - r) / d * p % (mi / d)
+    r += mM * tmp
+    mM *= mi / d
+  end
+
+  [r % mM, mM]
 end
 
 part1(input)
