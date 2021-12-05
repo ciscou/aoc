b10d9fe90b5ede86d6da3e314b3da411f5633e56
ciscou <francismpp@gmail.com>
2021-12-05 17:07:05 +0100

    aaand more refactoring

--- 2021/05.rb
+++ 2021/05.rb
@@ -2,22 +2,18 @@ INPUT = File.read(__FILE__.sub('.rb', '.txt')).lines.map(&:chomp)
 
 def solve(lines, ignore_diagonals)
   lines.each_with_object(Hash.new(0)) do |((x1, y1), (x2, y2)), diagram|
-    width, height = [(x2 - x1), (y2 - y1)]
+    next if ignore_diagonals && x1 != x2 && y1 != y2
 
-    next if ignore_diagonals && width != 0 && height != 0
+    width_and_height = [x2 - x1, y2 - y1]
+    dx, dy = width_and_height.map { |x| x <=> 0 }
 
-    dx, dy = [width <=> 0, height <=> 0]
-
-    max = [width, height].map(&:abs).max
-    (max + 1).times do |i|
+    (width_and_height.map(&:abs).max + 1).times do |i|
       diagram[[x1 + i * dx, y1 + i * dy]] += 1
     end
   end.select { |k, v| v > 1 }.length
 end
 
-lines = INPUT.map do |line|
-  line.split(" -> ").map { |p| p.split(",").map(&:to_i) }
-end
+lines = INPUT.map { |l| l.split(" -> ").map { |p| p.split(",").map(&:to_i) } }
 
 puts solve(lines, true)
 puts solve(lines, false)
