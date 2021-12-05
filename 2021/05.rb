f33cddbd831b4fafdb11b51fffa97677bdb89d05
ciscou <francismpp@gmail.com>
2021-12-05 15:10:44 +0100

    2021 05

---
+++ 2021/05.rb
@@ -0,0 +1,34 @@
+INPUT = File.read(__FILE__.sub('.rb', '.txt')).lines.map(&:chomp)
+
+def solve(lines, ignore_diagonals)
+  diagram = Hash.new(0)
+
+  lines.each do |line|
+    p1, p2 = line
+    x1, y1 = p1
+    x2, y2 = p2
+
+    dx = x2 - x1
+    dy = y2 - y1
+
+    if !ignore_diagonals || dx == 0 || dy == 0
+      loop do
+        diagram[[x1, y1]] += 1
+
+        break if x1 == x2 && y1 == y2
+
+        x1 += dx <=> 0
+        y1 += dy <=> 0
+      end
+    end
+  end
+
+  puts diagram.select { |k, v| v > 1 }.length
+end
+
+lines = INPUT.map do |line|
+  line.split(" -> ").map { |p| p.split(",").map(&:to_i) }
+end
+
+solve(lines, true)
+solve(lines, false)
