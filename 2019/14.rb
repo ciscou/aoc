11f0e27a8bdc1b04b977b9dd16769ebe6e236bde
ciscou <francismpp@gmail.com>
2021-12-09 23:35:13 +0100

    rewrite 2019 14

--- 2019/14.rb
+++ 2019/14.rb
@@ -1,79 +1,56 @@
-recipes = {}
-leftovers = {}
-leftovers.default = 0
+INPUT = File.read(__FILE__.sub('.rb', '.txt')).lines.map(&:chomp)
 
-while s = gets
-  s.chomp!
+REACTION_TO_PRODUCE = INPUT.each_with_object({}) do |line, h|
+  input, output = line.split(" => ")
+  output_quantity, output_name = output.split(" ")
 
-  input, output = s.split(" => ")
+  h[output_name] = {
+    inputs: input.split(", ").each_with_object({}) do |part, h|
+      quantity, name = part.split(" ")
 
-  ingredients = input.split(", ").map do |ingredient|
-    quantity, ingredient = ingredient.split(" ")
-    [quantity.to_i, ingredient]
-  end
-
-  quantity, recipe = output.split(" ")
-
-  recipes[recipe] = { ingredients: ingredients, quantity: quantity.to_i }
+      h[name] = quantity.to_i
+    end,
+    output: output_quantity.to_i
+  }
 end
 
-def calculate_ore_for(recipes, leftovers, recipe, quantity)
-  return quantity if recipe == "ORE"
+def calculate_ore_for(amount, chemical)
+  leftovers = Hash.new(0)
+  reaction = REACTION_TO_PRODUCE[chemical]
 
-  if leftovers[recipe] >= quantity
-    leftovers[recipe] -= quantity
-    0
-  else
-    quantity -= leftovers[recipe]
-    leftovers[recipe] = 0
+  inputs = reaction[:inputs].each_with_object(Hash.new(0)) { |(k, v), h| h[k] = v * amount }
 
-    r = recipes[recipe]
+  while input = inputs.detect { |k, v| k != "ORE" }
+    input_chemical, input_amount = input
+    inputs.delete(input_chemical)
 
+    lo = [input_amount, leftovers[input_chemical]].min
+    leftovers[input_chemical] -= lo
+    input_amount -= lo
 
-
-    res = 0
-    produced = 0
-
-    while produced < quantity
-      res += r[:ingredients].sum do |i|
-        calculate_ore_for(recipes, leftovers, i.last, i.first)
-      end
-      produced += r[:quantity]
+    r = REACTION_TO_PRODUCE[input_chemical]
+    n = (input_amount.fdiv(r[:output])).ceil
+    r[:inputs].each do |i|
+      c, a = i
+      inputs[c] += n * a
     end
+    leftovers[input_chemical] += n * r[:output] - input_amount
+  end
 
+  inputs["ORE"]
+end
 
+puts calculate_ore_for(1, "FUEL")
 
+low, high = 0, 1_000_000_000_000
+while (high - low) > 1
+  mid = (low + high) / 2
 
-#   n = (quantity.to_f / r[:quantity]).ceil
-#   res = n * r[:ingredients].sum do |i|
-#     calculate_ore_for(recipes, leftovers, i.last, i.first)
-#   end
-#   produced = n * r[:quantity]
-
-
-
-
-
-    leftovers[recipe] = produced - quantity
-
-    res
+  if calculate_ore_for(mid, "FUEL") > 1_000_000_000_000
+    high = mid
+  else
+    low = mid
   end
 end
 
-def maximize(recipes, leftovers, recipe, ore)
-  maximize2(recipes, leftovers, recipe, ore, 1, 100)
-end
-
-def maximize2(recipes, leftovers, recipe, ore, from, to)
-  puts calculate_ore_for(recipes, leftovers, recipe, from)
-  puts calculate_ore_for(recipes, leftovers, recipe, to)
-end
-
-puts calculate_ore_for(recipes, leftovers, "FUEL", 1)
-puts calculate_ore_for(recipes, leftovers, "FUEL", 10)
-puts calculate_ore_for(recipes, leftovers, "FUEL", 100)
-puts calculate_ore_for(recipes, leftovers, "FUEL", 1000)
-puts calculate_ore_for(recipes, leftovers, "FUEL", 10000)
-puts calculate_ore_for(recipes, leftovers, "FUEL", 100000)
-
-# puts maximize(recipes, leftovers, "FUEL", 1_000_000_000_000)
+p [low, high]
