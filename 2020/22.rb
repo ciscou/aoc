56f3d492af0fea2d78eb008a3d9ca492e9494a9e
ciscou <francismpp@gmail.com>
2021-12-11 04:28:57 +0100

    refactoring

--- 2020/22.rb
+++ 2020/22.rb
@@ -1,67 +1,11 @@
-INPUT = <<EOS
-Player 1:
-41
-26
-29
-11
-50
-38
-42
-20
-13
-9
-40
-43
-10
-24
-35
-30
-23
-15
-31
-48
-27
-44
-16
-12
-14
-
-Player 2:
-18
-6
-32
-37
-25
-21
-33
-28
-7
-8
-45
-46
-49
-5
-19
-2
-39
-4
-17
-3
-22
-1
-34
-36
-47
-EOS
+INPUT = File.read(__FILE__.sub('.rb', '.txt')).lines.map(&:chomp)
 
 def prepare_stacks
   p1 = []
   p2 = []
   pc = nil
 
-  INPUT.lines.each do |line|
-    line.chomp!
-
+  INPUT.each do |line|
     next if line.empty?
 
     case line
@@ -77,10 +21,7 @@ end
 def combat(p1, p2, recursive: false)
   seen = {}
 
-  round = 0
   while [p1, p2].none?(&:empty?)
-    round += 1
-
     if seen[[p1, p2].map { |p| p.join(",") }.join(";")]
       return p1
     end
@@ -90,20 +31,20 @@ def combat(p1, p2, recursive: false)
     c1 = p1.shift
     c2 = p2.shift
 
-    if recursive && p1.length >= c1 && p2.length >= c2
-      newp1 = p1[0, c1].map(&:itself)
-      newp2 = p2[0, c2].map(&:itself)
+    winner = if recursive && p1.length >= c1 && p2.length >= c2
+               newp1 = p1[0, c1]
+               newp2 = p2[0, c2]
 
-      winner = combat(newp1, newp2, recursive: recursive)
+               winner = combat(newp1, newp2, recursive: recursive)
 
-      if winner == newp1
-        p1.push(c1)
-        p1.push(c2)
-      else
-        p2.push(c2)
-        p2.push(c1)
-      end
-    elsif c1 > c2
+               winner == newp1 ? p1 : p2
+             elsif c1 > c2
+               p1
+             else
+               p2
+             end
+
+    if winner == p1
       p1.push(c1)
       p1.push(c2)
     else
