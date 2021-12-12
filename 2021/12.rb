4fb1622d35eba1288c2e6940ab864a098a1b18c0
ciscou <francismpp@gmail.com>
2021-12-12 16:19:46 +0100

    refactor

--- 2021/12.rb
+++ 2021/12.rb
@@ -17,23 +17,20 @@ def find_all_paths(make_an_exception)
     node = stack.pop
     cave, path, visited, exception = node
 
-    count_caves = path.group_by(&:itself)
-    lowercase_caves = path.select { |x| x.downcase == x && x != 'start' && x != 'end' }
-
     if cave == 'end'
       res << path
       next
     end
 
     ADJACENTS[cave].each do |next_cave|
-      next_visited = visited.merge(next_cave => next_cave.downcase == next_cave)
       next_path = path + [next_cave]
+      next_visited = visited.merge(next_cave => next_cave.downcase == next_cave)
+      next_exception = exception || visited[next_cave]
+
       can_make_an_exception = make_an_exception && !exception && next_cave != 'start' && next_cave != 'end'
 
-      if !visited[next_cave]
-        stack.push([next_cave, next_path, next_visited, exception])
-      elsif can_make_an_exception
-        stack.push([next_cave, next_path, next_visited, true])
+      if !visited[next_cave] || can_make_an_exception
+        stack.push([next_cave, next_path, next_visited, next_exception])
       end
     end
   end
