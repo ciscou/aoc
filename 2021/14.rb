d14a1fcf35d135f9e4779639fa32743719680e22
ciscou <francismpp@gmail.com>
2021-12-15 19:10:25 +0100

    2021 14 part2 (this has been harder than it should)

--- 2021/14.rb
+++ 2021/14.rb
@@ -4,7 +4,7 @@ TEMPLATE = INPUT.first
 
 RULES = Hash[INPUT[2..-1].map { |l| l.split(" -> ") }]
 
-def evolve(polymer)
+def evolve_polymer(polymer)
   last_element = nil
 
   polymer = polymer.each_cons(2).map do |a, b|
@@ -16,24 +16,60 @@ def evolve(polymer)
   polymer << last_element
 end
 
-def substract(polymer)
-  minmax = polymer.tally.minmax { |a, b| a.last <=> b.last }
+def substract(elements_counts)
+  minmax = elements_counts.minmax { |a, b| a.last <=> b.last }
 
   minmax.last.last - minmax.first.last
 end
 
-polymer = TEMPLATE.chars
+def evolve_polymer_pairs_and_elements_counts(polymer_pairs, elements_counts)
+  next_polymer_pairs = Hash.new(0)
+  next_elements_counts = elements_counts.dup
 
-stats = [polymer.tally]
+  polymer_pairs.each do |k, v|
+    a, c = k.chars
+    b = RULES[k]
 
-15.times do |i|
-  polymer = evolve(polymer)
-  stats << polymer.tally
+    next_polymer_pairs["#{a}#{b}"] += v
+    next_polymer_pairs["#{b}#{c}"] += v
+
+    next_elements_counts[b] += v
+  end
+
+  [next_polymer_pairs, next_elements_counts]
 end
 
-elements = stats.first.map(&:first)
-puts "  " + elements.map { |e| e.ljust(8, " ") }.join(" | ")
+def part1
+  polymer = TEMPLATE.chars
 
-stats.each do |stat|
-  puts "  " + elements.map { |e| stat[e].to_s.rjust(8, " ") }.join(" | ")
+  10.times do |i|
+    polymer = evolve_polymer(polymer)
+  end
+
+  substract(polymer.tally)
 end
+
+def part2
+  polymer = TEMPLATE.chars
+
+  polymer_pairs = Hash.new(0)
+
+  polymer.each_cons(2).each do |a, b|
+    polymer_pairs["#{a}#{b}"] += 1
+  end
+
+  elements_counts = Hash.new(0)
+
+  polymer.each do |e|
+    elements_counts[e] += 1
+  end
+
+  40.times do |i|
+    polymer_pairs, elements_counts = evolve_polymer_pairs_and_elements_counts(polymer_pairs, elements_counts)
+  end
+
+  substract(elements_counts)
+end
+
+puts part1
+puts part2
