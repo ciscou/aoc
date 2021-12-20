b874e7be6558861aee4e63b65401ecdbd820eddc
ciscou <francismpp@gmail.com>
2021-12-20 09:38:01 +0100

    wip2

--- 2021/19.rb
+++ 2021/19.rb
@@ -1,71 +1,136 @@
 INPUT = File.read(ARGV.first || __FILE__.sub('.rb', '.txt')).lines.map(&:chomp)
 
-def all_24_rotations_bak(pos)
-  x, y, z = pos
+# def rx(pos)
+#   x, y, z = pos
+#   [x, -z, y]
+# end
 
-  [
-    [ x,  y,  z],
-    [ x,  y, -z],
-    [ x, -y,  z],
-    [ x, -y, -z],
+# def ry(pos)
+#   x, y, z = pos
+#   [z, y, -x]
+# end
 
-    [ x,  z,  y],
-    [ x,  z, -y],
-    [ x, -z,  y],
-    [ x, -z, -y],
+# def rz(pos)
+#   x, y, z = pos
+#   [-y, x, z]
+# end
 
-    [  x, y,  z],
-    [  x, y, -z],
-    [ -x, y,  z],
-    [ -x, y, -z],
+# def all_24_rotations_generator(pos)
+#   rotations = []
 
-    [  z, y,  x],
-    [  z, y, -x],
-    [ -z, y,  x],
-    [ -z, y, -x],
+#   4.times do |x|
+#     4.times do |y|
+#       4.times do |z|
+#         x.times { pos = rx(pos) }
+#         y.times { pos = ry(pos) }
+#         z.times { pos = rz(pos) }
 
-    [  x,  y, z],
-    [  x, -y, z],
-    [ -x,  y, z],
-    [ -x, -y, z],
+#         rotations << pos
+#       end
+#     end
+#   end
 
-    [  y,  x, z],
-    [  y, -x, z],
-    [ -y,  x, z],
-    [ -y, -x, z]
-  ]
-end
+#   4.times do |x|
+#     4.times do |y|
+#       4.times do |z|
+#         x.times { pos = rx(pos) }
+#         z.times { pos = rz(pos) }
+#         y.times { pos = ry(pos) }
+
+#         rotations << pos
+#       end
+#     end
+#   end
+
+#   4.times do |x|
+#     4.times do |y|
+#       4.times do |z|
+#         y.times { pos = ry(pos) }
+#         x.times { pos = rx(pos) }
+#         z.times { pos = rz(pos) }
+
+#         rotations << pos
+#       end
+#     end
+#   end
+
+#   4.times do |x|
+#     4.times do |y|
+#       4.times do |z|
+#         y.times { pos = ry(pos) }
+#         z.times { pos = rz(pos) }
+#         x.times { pos = rx(pos) }
+
+#         rotations << pos
+#       end
+#     end
+#   end
+
+#   4.times do |x|
+#     4.times do |y|
+#       4.times do |z|
+#         z.times { pos = rz(pos) }
+#         x.times { pos = rx(pos) }
+#         y.times { pos = ry(pos) }
+
+#         rotations << pos
+#       end
+#     end
+#   end
+
+#   4.times do |x|
+#     4.times do |y|
+#       4.times do |z|
+#         z.times { pos = rz(pos) }
+#         y.times { pos = ry(pos) }
+#         x.times { pos = rx(pos) }
+
+#         rotations << pos
+#       end
+#     end
+#   end
+
+#   rotations.sort.uniq
+# end
+
+# puts "before"
+# puts all_24_rotations([1, 2, 3]).map(&:inspect)
+# puts "after"
+# exit(0)
 
 def all_24_rotations(pos)
   x, y, z = pos
 
   [
-    [x, y, z],
-    [x, -z, y],
-    [x, -y, -z],
-    [x, z, -y],
-    [x, y, z],
-    [z, y, -x],
-    [z, x, y],
-    [z, -y, x],
-    [z, -x, -y],
-    [z, y, -x],
-    [-x, y, -z],
-    [-x, z, y],
-    [-x, -y, z],
-    [-x, -z, -y],
-    [-x, y, -z],
-    [-z, y, x],
-    [-z, -x, y],
     [-z, -y, -x],
+    [-z, -x, y],
     [-z, x, -y],
     [-z, y, x],
-    [x, y, z],
+    [-y, -z, x],
+    [-y, -x, -z],
+    [-y, x, z],
+    [-y, z, -x],
+    [-x, -z, -y],
+    [-x, -y, z],
+    [-x, y, -z],
+    [-x, z, y],
     [x, -z, y],
     [x, -y, -z],
+    [x, y, z],
     [x, z, -y],
+    [y, -z, -x],
+    [y, -x, z],
+    [y, x, -z],
+    [y, z, x],
+    [z, -y, x],
+    [z, -x, -y],
+    [z, x, y],
+    [z, y, -x]
   ]
 end
+
+# puts all_24_rotations([1, 2, 3]).map(&:inspect)
+# exit(0)
 
 reports = Hash.new { |h, k| h[k] = [] }
 scanner = nil
@@ -84,41 +149,34 @@ INPUT.each do |line|
 end
 
 reports.each do |scanner_1, positions_1|
+  # puts scanner_1
   reports.each do |scanner_2, positions_2|
     next unless scanner_2 > scanner_1
 
     matches = nil
 
-    positions_1.each do |pos1|
-      positions_2.each do |pos2|
-        # let's assume pos1 and pos2 are the same point from different scanners
-        # let's count coincidences
+    24.times do |rotation|
+      rotated_positions_2 = positions_2.map { |x, y, z| all_24_rotations([x, y, z])[rotation] }
 
-        x1, y1, z1 = pos1
-        24.times do |rotation|
-          rotated_pos2 = all_24_rotations(pos2)[rotation]
-          x2, y2, z2 = rotated_pos2
+      positions_1.each.with_index do |pos1, i|
+        rotated_positions_2.each.with_index do |pos2, j|
+          next unless j > i
+
+          # let's assume pos1 and pos2 are the same point from different scanners
+          # let's count coincidences
+
+          x1, y1, z1 = pos1
+          x2, y2, z2 = pos2
           ox, oy, oz = x2 - x1, y2 - y1, z2 - z1
 
-          translated_positions_2 = positions_2.map { |x, y, z| all_24_rotations([x, y, z])[rotation] }.map { |x, y, z| [x - ox, y - oy, z - oz] }
+          rotated_translated_positions_2 = rotated_positions_2.map { |x, y, z| [x - ox, y - oy, z - oz] }
 
-          # if rotation == 11 && scanner_1 == 0 && scanner_2 == 1 && pos1 == [-618,-824,-621] && pos2 == [686,422,578]
-          #   p [:pos1, pos1]
-          #   p [:pos2, pos2]
-          #   p [:rotated_pos2, rotated_pos2]
-          #   p [:offsets, [ox, oy, oz]]
-          #   p [:rotated_translated, [x2 - ox, y2 - oy, z2 - oz]]
-          #   p [:positions_1, positions_1]
-          #   p [:positions_2, translated_positions_2]
-          #   p [:intersection, positions_1 & translated_positions_2]
-          #   $stdin.gets
-          # end
-
-          matches = [ox, oy, oz] if (positions_1 & translated_positions_2).size >= 12
+          count = (positions_1 & rotated_translated_positions_2).size
+          matches = [rotation, ox, oy, oz] if count >= 12
         end
       end
     end
 
-    p [scanner_1, scanner_2, matches] if matches
+    puts [scanner_1, scanner_2, matches].inspect if matches
   end
 end
