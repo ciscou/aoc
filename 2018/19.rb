424928779cc64ef29a04234b1e3406b22dd8d0ba
ciscou <francismpp@gmail.com>
2021-12-26 23:17:37 +0100

    simplifying

--- 2018/19.rb
+++ 2018/19.rb
@@ -6,21 +6,19 @@ r2 = 2 * 2 * 19 * 11 + 6 * 22 + 8
 r2 += (27 * 28 + 29) * 30 * 14 * 32 if part2
 
 r1 = 1
-loop do
+
+until r1 > r2
   r5 = 1
-  loop do
-    r3 = r1 * r5
-    r3 = r3 == r2 ? 1 : 0
-    if r3 == 1
+
+  until r5 > r2
+    if r1 * r5 == r2
       r0 += r1
     end
+
     r5 += 1
-    r3 = r5 > r2 ? 1 : 0
-    break if r3 == 1
   end
+
   r1 += 1
-  r3 = r1 > r2 ? 1 : 0
-  break if r3 == 1
 end
 
 puts r0
