d49ce37c2996b33ebd480b221e282b800c957e9e
ciscou <francismpp@gmail.com>
2021-12-27 15:17:00 +0100

    simplifying as much as I can

--- 2018/21rb.rb
+++ 2018/21rb.rb
@@ -1,30 +1,16 @@
-r0 = r1 = r2 = r3 = r4 = r5 = 0
+r3 = r5 = 0
 r0 = 9959629
 
-loop do
+begin
   r5 = r3 | 65536                  # 06
   r3 = 5557974                     # 07
-  loop do
-    r2 = r5 & 255                  # 08
-    r3 += r2                       # 09
+  begin
+    r3 += r5 & 255                 # 08, 09
     r3 &= 16777215                 # 10
     r3 *= 65899                    # 11
     r3 &= 16777215                 # 12
-    puts "uno 256, r5=#{r5}"
-    r2 = 256 > r5 ? 1 : 0          # 13
-    break if r2 == 1               # 14
-    r2 = 0                         # 17
-    loop do
-      r1 = r2 + 1                  # 18
-      r1 *= 256                    # 19
-      puts "dos r1=#{r1}, r5=#{r5}"
-      r1 = r1 > r5 ? 1 : 0         # 20
-      break if r1 == 1             # 21
-      r2 += 1                      # 24
-    end                            # 25
-    r5 = r2                        # 26
-  end                              # 27
-  puts "tres r3=#{r3}, r0=#{r0}"
-  r2 = r3 == r0 ? 1 : 0            # 28
-  break if r2 == 1                 # 29
-end                                # 30
+    r5 /= 256                      # 17-26
+    puts "uno #{r5} == 0"
+  end until r5 == 0                # 27
+  puts "dos #{r3} == #{r0}"
+end until r3 == r0                 # 28, 29, 30
