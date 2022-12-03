872907bbcc58a4f78c286e143fe67d030f415666
ciscou <francismpp@gmail.com>
2022-12-03 11:31:29 +0100

    refactor

--- 2022/03.rb
+++ 2022/03.rb
@@ -1,29 +1,20 @@
 INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 
-rucksacks = INPUT.map do |line|
-  compartiment_size = line.length / 2
-
-  [line[0, compartiment_size], line[compartiment_size, compartiment_size]].map(&:chars)
-end
-
-part1 = rucksacks.sum do |compartiment1, compartiment2|
-  common = (compartiment1 & compartiment2).first
-
-  case common
-  when 'a'..'z' then common.ord - 'a'.ord + 1
-  when 'A'..'Z' then common.ord - 'A'.ord + 27
+def priority(item_type)
+  case item_type
+  when 'a'..'z' then item_type.ord - 'a'.ord + 1
+  when 'A'..'Z' then item_type.ord - 'A'.ord + 27
   end
 end
 
+rucksacks = INPUT.map(&:chars)
+
+part1 = rucksacks.sum do |rucksack|
+  priority(rucksack.each_slice(rucksack.length / 2).reduce(:&).first)
+end
 puts part1
 
 part2 = rucksacks.each_slice(3).sum do |group|
-  common = group.map { |c1, c2| c1 + c2 }.reduce(&:&).first
-
-  case common
-  when 'a'..'z' then common.ord - 'a'.ord + 1
-  when 'A'..'Z' then common.ord - 'A'.ord + 27
-  end
+  priority(group.reduce(:&).first)
 end
-
 puts part2
