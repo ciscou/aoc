305a59e42f576b3f400cfe7c945c21eceabb6443
ciscou <francismpp@gmail.com>
2022-12-04 13:27:21 +0100

    no need to chomp

--- 2022/04p1.rb
+++ 2022/04p1.rb
@@ -1,1 +1,1 @@
-p File.readlines('04.txt',chomp:1).count{r,s=_1.split(',').map{|r|l,h=r.split('-');l.to_i..h.to_i};r.cover?(s)||s.cover?(r)}
+p File.readlines('04.txt').count{r,s=_1.split(',').map{|r|l,h=r.split('-');l.to_i..h.to_i};r.cover?(s)||s.cover?(r)}
