12c26d8dac1de9fda2699377a058178c1199fc5d
ciscou <francismpp@gmail.com>
2022-12-04 11:14:28 +0100

    this is actually shorter

--- 2022/04p2.rb
+++ 2022/04p2.rb
@@ -1,1 +1,1 @@
-p File.readlines('04.txt',chomp:1).count{|l|r,s=l.split(',').map{|r|l,h=r.split('-').map(&:to_i);l..h};r.cover?(s.first)||s.cover?(r.first)}
+p File.readlines('04.txt',chomp:1).count{|l|r,s=l.split(',').map{|r|l,h=r.split('-');l.to_i..h.to_i};r.cover?(s.first)||s.cover?(r.first)}
