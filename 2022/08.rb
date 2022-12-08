0355c0f6598493b318f9fb23e8917da638630a95
ciscou <francismpp@gmail.com>
2022-12-08 10:06:59 +0100

    2022 08 p2

--- 2022/08.rb
+++ 2022/08.rb
@@ -23,7 +23,7 @@ visible = Set.new
 
 HEIGHT.times do |row|
   [
-    [row, 0, 0, 1],
+    [row,       0, 0,  1],
     [row, WIDTH-1, 0, -1],
   ].each do |r, c, dr, dc|
     xray(r, c, dr, dc, visible)
@@ -32,7 +32,7 @@ end
 
 WIDTH.times do |col|
   [
-    [0, col, 1, 0],
+    [       0, col,  1, 0],
     [HEIGHT-1, col, -1, 0],
   ].each do |r, c, dr, dc|
     xray(r, c, dr, dc, visible)
@@ -40,3 +40,39 @@ WIDTH.times do |col|
 end
 
 puts visible.size
+
+def scenic_score_helper(r, c, dr, dc)
+  res = 0
+  highest = INPUT[r][c]
+
+  r += dr
+  c += dc
+
+  while r >= 0 && c >= 0 && r < HEIGHT && c < WIDTH
+    res += 1
+
+    break unless INPUT[r][c] < highest
+
+    r += dr
+    c += dc
+  end
+
+  res
+end
+
+def scenic_score(r, c)
+  scenic_score_helper(r, c,  1,  0) *
+  scenic_score_helper(r, c, -1,  0) *
+  scenic_score_helper(r, c,  0,  1) *
+  scenic_score_helper(r, c,  0, -1)
+end
+
+highest_scenic_score = -1
+
+HEIGHT.times do |r|
+  WIDTH.times do |c|
+    highest_scenic_score = [highest_scenic_score, scenic_score(r, c)].max
+  end
+end
+
+puts highest_scenic_score
