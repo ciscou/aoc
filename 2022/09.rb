80e46a8889a89c57133be39eab6f13daf2209a72
ciscou <francismpp@gmail.com>
2022-12-09 08:22:46 +0100

    refactor

--- 2022/09.rb
+++ 2022/09.rb
@@ -2,36 +2,44 @@ require 'set'
 
 INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 
-def simulate(knots)
-  rope = knots.times.map { [0, 0] }
-
-  visited = Set.new
-  visited.add(rope.last)
-
-  INPUT.each do |line|
-    dir, count = line.split
-    move = case dir
+def move(head, dir)
+  hx, hy = head
+  dx, dy = case dir
            when "U" then [ 0, -1]
            when "D" then [ 0,  1]
            when "R" then [ 1,  0]
            when "L" then [-1,  0]
            end
 
+  [hx + dx, hy + dy]
+end
+
+def follow(knot1, knot2)
+  dist = knot1.zip(knot2).map { |a, b| a - b }
+
+  if dist.all? { _1.abs < 2 }
+    knot2
+  else
+    knot2.zip(dist).map { |a, b| a + (b <=> 0) }
+  end
+end
+
+def simulate(knots)
+  rope = Array.new(knots) { [0, 0] }
+
+  visited = Set.new
+  visited.add(rope.last)
+
+  INPUT.each do |line|
+    dir, count = line.split
+
     count.to_i.times do
-      head = rope.first
-      head = head.zip(move).map { |a, b| a + b }
-
-      rope[0] = head
-
-      (1...rope.length).each do |tidx|
-        h = rope[tidx - 1]
-        t = rope[tidx]
-        dist = h.zip(t).map { |a, b| a - b }
-
-        touching = dist.all? { _1.abs < 2 }
-        t = t.zip(dist).map { |a, b| a + (b <=> 0) } unless touching
-
-        rope[tidx] = t
+      rope.length.times do |idx|
+        rope[idx] = if idx < 1
+                      move(rope[idx], dir)
+                    else
+                      follow(rope[idx-1], rope[idx])
+                    end
       end
 
       visited.add(rope.last)
