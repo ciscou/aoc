36e9194ba3e3206213251ef7762c31daac9cd6c3
ciscou <francismpp@gmail.com>
2022-12-10 10:30:47 +0100

    2022 10 p2 with off-by-one error (?)

--- 2022/10.rb
+++ 2022/10.rb
@@ -3,10 +3,12 @@ INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 x = 1
 cycle = 0
 signal_strengths = []
+crt = []
 
 INPUT.each do |line|
   cycle += 1
   signal_strengths << cycle * x if (cycle - 20) % 40 == 0
+  crt << ((((crt.length - 1) % 40) - (x - 1)).abs < 2)
 
   instruction, argument = line.split
 
@@ -15,6 +17,7 @@ INPUT.each do |line|
   when "addx"
     cycle += 1
     signal_strengths << cycle * x if (cycle - 20) % 40 == 0
+    crt << ((((crt.length - 1) % 40) - (x - 1)).abs < 2)
 
     x += argument.to_i
   else
@@ -23,3 +26,6 @@ INPUT.each do |line|
 end
 
 puts signal_strengths.sum
+crt.each_slice(40).each do |row|
+  puts row.map { |pixel| pixel ? "#" : " " }.join
+end
