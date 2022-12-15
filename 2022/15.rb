a13be13e3211a6b197982925a0b37614656b7968
ciscou <francismpp@gmail.com>
2022-12-15 10:14:47 +0100

    2022 15 p2

--- 2022/15.rb
+++ 2022/15.rb
@@ -8,23 +8,54 @@ sensors = INPUT.map do |line|
   [$1, $2, $3, $4].map(&:to_i)
 end
 
-grid = Hash.new { |h, k| h[k] = {} }
+def merge_intervals(intervals)
+  intervals.sort_by!(&:first)
+  res = [intervals.first]
 
-sensors.each do |sensor|
-  sx, sy, bx, by = sensor
-
-  dx = (bx - sx).abs
-  dy = (by - sy).abs
-
-  md = dx + dy
-
-  oy = 2_000_000 - sy
-  width = (md - oy.abs)
-  (-width..width).each do |ox|
-    x, y = sx + ox, sy + oy
-    grid[y][x] = true
+  intervals.each do |a, b|
+    if res.last[1] >= a
+      res.last[1] = b if b > res.last[1]
+    else
+      res.push([a, b])
+    end
   end
+
+  res
 end
 
-beacons = sensors.map { |_, _, x, y| [x, y] }.select { |_, y| y == 2_000_000 }.uniq
-puts grid[2_000_000].size - beacons.size
+def intervals_at_row(sensors, row)
+  intervals = []
+
+  sensors.each do |sensor|
+    sx, sy, bx, by = sensor
+
+    dx = (bx - sx).abs
+    dy = (by - sy).abs
+
+    md = dx + dy
+
+    oy = row - sy
+    width = (md - oy.abs)
+    if width > 0
+      intervals << [sx-width, sx+width]
+    end
+  end
+
+  merge_intervals(intervals)
+end
+
+row = 2_000_000
+beacons = sensors.map { |_, _, x, y| [x, y] }.select { |_, y| y == row }.uniq
+puts merge_intervals(intervals_at_row(sensors, row)).sum { |a, b| b - a + 1 } - beacons.size
+
+max = 4_000_000
+(0..max).each do |y|
+  intervals_at_row(sensors, y).each_cons(2).each do |i1, i2|
+    i1last = i1.last
+    i2first = i2.first
+
+    if i2first > i1last + 1 && i2first < max
+      puts (i2first - 1) * 4_000_000 + y
+    end
+  end
+end
