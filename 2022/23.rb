1137d28891d5cb985b3102f17409b094853e8813
ciscou <francismpp@gmail.com>
2022-12-23 11:03:30 +0100

    2022 23 p2

--- 2022/23.rb
+++ 2022/23.rb
@@ -6,9 +6,7 @@ elves = []
 
 INPUT.each_with_index do |line, r|
   line.chars.each_with_index do |char, c|
-    if char == "#"
-      elves << { pos: [r, c] }
-    end
+    elves << { pos: [r, c] } if char == "#"
   end
 end
 
@@ -19,45 +17,31 @@ decisions = [
   [[-1,  1], [ 0,  1], [ 1,  1]],
 ]
 
-11.times do
+rounds = 0
+
+loop do
+  rounds += 1
+
   curr_pos = Set.new(elves.map { _1[:pos] })
   next_pos = Hash.new(0)
-
-  minx, maxx = elves.map { _1[:pos][0] }.minmax
-  miny, maxy = elves.map { _1[:pos][1] }.minmax
-  puts (maxx - minx + 1) * (maxy - miny + 1) - elves.size
-  # minx -= 1
-  # maxx += 1
-  # miny -= 1
-  # maxx += 1
-  # (minx..maxx).each do |y|
-  #   (miny..maxy).each do |x|
-  #     print curr_pos.include?([y, x]) ? "#" : "."
-  #   end
-  #   puts
-  # end
-  # puts
-  # gets
 
   elves.each do |elf|
     r, c = elf[:pos]
 
-    if [
+    next if [
       [r-1, c-1],
       [r-1, c  ],
       [r-1, c+1],
       [r  , c-1],
-      # [r  , c  ],
+    # [r  , c  ],
       [r  , c+1],
       [r+1, c-1],
       [r+1, c  ],
       [r+1, c+1],
     ].none? { curr_pos.include?(_1) }
-      # puts "#{elf.inspect} choose not to move"
-      next
-    end
 
-    decisions.each do |decision|
+    4.times do |i|
+      decision = decisions[(rounds - 1 + i) % 4]
       d1, d2, d3 = decision.map { [r + _1, c + _2] }
 
       elf[:next_pos] ||= d2 if [d1, d2, d3].none? { curr_pos.include?(_1) }
@@ -66,16 +50,24 @@ decisions = [
     next_pos[elf[:next_pos]] += 1 if elf[:next_pos]
   end
 
+  no_elf_moved = true
   elves.each do |elf|
     if elf[:next_pos] && next_pos[elf[:next_pos]] == 1
+      no_elf_moved = false
       elf[:pos] = elf[:next_pos]
     end
 
     elf.delete(:next_pos)
   end
 
-  decisions.push(decisions.shift)
+  if rounds == 10
+    minx, maxx = elves.map { _1[:pos][0] }.minmax
+    miny, maxy = elves.map { _1[:pos][1] }.minmax
+
+    puts (maxx - minx + 1) * (maxy - miny + 1) - elves.size
+  end
+
+  break if no_elf_moved
 end
 
-minx, maxx = elves.map { _1[:pos][0] }.minmax
-miny, maxy = elves.map { _1[:pos][1] }.minmax
+puts rounds
