d4dbd7f9662dede995f3650ef5a34572417fe678
Francis <francismpp@gmail.com>
2023-12-03 11:29:07 +0100

    2023 03 p2

--- 2023/03.rb
+++ 2023/03.rb
@@ -1,15 +1,22 @@
 INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 
-def symbol_around?(grid, x, y)
+def symbols_around(grid, x, y)
+  res = []
+
   [-1, 0, 1].each do |dx|
     [-1, 0, 1].each do |dy|
-      unless %w[0 1 2 3 4 5 6 7 8 9 .].include?(grid[[x + dx, y + dy]])
-        return true
+      char = grid[[x + dx, y + dy]]
+      unless %w[0 1 2 3 4 5 6 7 8 9 .].include?(char)
+        res << {
+          char: char,
+          x: x + dx,
+          y: y + dy,
+        }
       end
     end
   end
 
-  false
+  res
 end
 
 grid = Hash.new(".")
@@ -20,23 +27,40 @@ INPUT.each_with_index do |line, y|
 end
 
 part1 = 0
+symbols = Hash.new { |h, k| h[k] = { parts: [] } }
 
 INPUT.length.times do |y|
   n = 0
-  adj = false
+  adj = Set.new
 
   (INPUT.first.length + 1).times do |x| # +1 to include right border numbers
     char = grid[[x, y]]
     if ("0".."9").include? char
-      adj ||= symbol_around?(grid, x, y)
+      adj += symbols_around(grid, x, y)
       n *= 10
       n += char.to_i
     else
-      part1 += n if adj
+      part1 += n unless adj.empty?
+      if n > 0
+        adj.each do |sym|
+          symbols[[sym[:x], sym[:y]]][:char] = sym[:char]
+          symbols[[sym[:x], sym[:y]]][:parts] << n
+        end
+      end
       n = 0
-      adj = false
+      adj = Set.new
     end
   end
 end
 
 puts part1
+
+part2 = 0
+
+symbols.each_value do |sym|
+  if sym[:char] == "*" && sym[:parts].length == 2
+    part2 += sym[:parts].reduce(:*)
+  end
+end
+
+puts part2
