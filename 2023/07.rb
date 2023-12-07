f8b77dee12124b8a64d522a56d6a5a72cb582233
Francis <francismpp@gmail.com>
2023-12-07 22:06:04 +0100

    2023 07 p2

--- 2023/07.rb
+++ 2023/07.rb
@@ -1,4 +1,5 @@
 INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
+PART2 = true
 
 class Card
   def initialize(value, suit)
@@ -11,7 +12,7 @@ class Card
   def numerical_value
     case @value
     when "T" then 10
-    when "J" then 11
+    when "J" then PART2 ? 1 : 11
     when "Q" then 12
     when "K" then 13
     when "A" then 14
@@ -53,28 +54,83 @@ class PokerHand
     [kind_power] + cards.map(&:numerical_value)
   end
 
+  def kind
+    if five_of_a_kind?
+      :five_of_a_kind
+    elsif four_of_a_kind?
+      :four_of_a_kind
+    elsif full_house?
+      :full_house
+    elsif three_of_a_kind?
+      :three_of_a_kind
+    elsif two_pairs?
+      :two_pairs
+    elsif pair?
+      :pair
+    elsif high_card?
+      :high
+    else
+      raise "WTF (What a Terrible Failure)"
+    end
+  end
+
+  def njokers
+    PART2 ? @cards.count { _1.value == "J" } : 0
+  end
+
+  def for_any_joker_combination?
+    res = false
+    original_cards = @cards
+    %w[A K Q T 9 8 7 6 5 4 3 2].repeated_combination(njokers) do |extra_cards|
+      @cards = @cards.reject { _1.value == "J" } if PART2
+      @cards += extra_cards.map { Card.new(_1, "X") }
+      res ||= yield
+      @cards = original_cards
+    end
+    res
+  end
+
   def five_of_a_kind?
-    @cards.group_by(&:value).select { |k, v| v.length == 5 }.length == 1
+    return true if njokers > 3
+
+    for_any_joker_combination? do
+      @cards.group_by(&:value).select { |k, v| v.length == 5 }.length == 1
+    end
   end
 
   def four_of_a_kind?
-    @cards.group_by(&:value).select { |k, v| v.length == 4 }.length == 1
+    return true if njokers > 2
+
+    for_any_joker_combination? do
+      @cards.group_by(&:value).select { |k, v| v.length == 4 }.length == 1
+    end
   end
 
   def full_house?
-    three_of_a_kind? && pair?
+    for_any_joker_combination? do
+      cards_by_value = @cards.group_by(&:value)
+      three = cards_by_value.select { |k, v| v.length == 3 }.length == 1
+      two = cards_by_value.select { |k, v| v.length == 2 }.length == 1
+      three && two
+    end
   end
 
   def three_of_a_kind?
-    @cards.group_by(&:value).select { |k, v| v.length == 3 }.length == 1
+    for_any_joker_combination? do
+      @cards.group_by(&:value).select { |k, v| v.length == 3 }.length == 1
+    end
   end
 
   def two_pairs?
-    @cards.group_by(&:value).select { |k, v| v.length == 2 }.length == 2
+    for_any_joker_combination? do
+      @cards.group_by(&:value).select { |k, v| v.length == 2 }.length == 2
+    end
   end
 
   def pair?
-    @cards.group_by(&:value).select { |k, v| v.length == 2 }.length == 1
+    for_any_joker_combination? do
+      @cards.group_by(&:value).select { |k, v| v.length == 2 }.length == 1
+    end
   end
 
   def high_card?
@@ -95,11 +151,9 @@ HANDS_AND_BIDS = INPUT.map do |line|
   ]
 end
 
-part1 = 0
+res = 0
 HANDS_AND_BIDS.sort_by! { _1.first.power }
 HANDS_AND_BIDS.each_with_index do |hand_and_bid, i|
-  hand, bid = hand_and_bid
-  p [hand.power, i+1, bid]
-  part1 += (i + 1) * bid
+  res += (i + 1) * hand_and_bid.last
 end
-puts part1
+puts res
