8211edc810215b99993617b024ed9d92e8055511
Francis <francismpp@gmail.com>
2023-12-10 15:52:50 +0100

    2023 10 p2

--- 2023/10.rb
+++ 2023/10.rb
@@ -22,10 +22,7 @@ INPUT.each_with_index do |line, y|
   end
 end
 
-def calculate_max_dist(grid, pos)
-  dist = {}
-  dist[pos] = 0
-
+def calculate_max_dist(grid, dist, pos)
   queue = [pos]
   max_dist = 0
 
@@ -44,5 +41,99 @@ def calculate_max_dist(grid, pos)
   max_dist
 end
 
-part1 = calculate_max_dist(grid, start)
+dist = {}
+dist[start] = 0
+
+part1 = calculate_max_dist(grid, dist, start)
 puts part1
+
+upscaled_grid = {}
+
+INPUT.each_with_index do |line, y|
+  line.chars.each_with_index do |char, x|
+    cell = if dist[[x, y]]
+             case char
+             when "|"
+               [
+                 [" ", "#", " "],
+                 [" ", "#", " "],
+                 [" ", "#", " "],
+               ]
+             when "-"
+               [
+                 [" ", " ", " "],
+                 ["#", "#", "#"],
+                 [" ", " ", " "],
+               ]
+             when "F", "S" # TODO: this S depends on the input
+               [
+                 [" ", " ", " "],
+                 [" ", "#", "#"],
+                 [" ", "#", " "],
+               ]
+             when "L"
+               [
+                 [" ", "#", " "],
+                 [" ", "#", "#"],
+                 [" ", " ", " "],
+               ]
+             when "J"
+               [
+                 [" ", "#", " "],
+                 ["#", "#", " "],
+                 [" ", " ", " "],
+               ]
+             when "7"
+               [
+                 [" ", " ", " "],
+                 ["#", "#", " "],
+                 [" ", "#", " "],
+               ]
+             else
+               raise "unknown char #{char}"
+             end
+           else
+             [
+               [" ", " ", " "],
+               [" ", ".", " "],
+               [" ", " ", " "],
+             ]
+           end
+
+    3.times do |dx|
+      3.times do |dy|
+        upscaled_grid[[3 * x + dx, 3 * y + dy]] = cell[dy][dx]
+      end
+    end
+  end
+end
+
+# minx, maxx = upscaled_grid.keys.map(&:first).minmax
+# miny, maxy = upscaled_grid.keys.map(&:last).minmax
+# (miny..maxy).each do |y|
+#   (minx..maxx).each do |x|
+#     print upscaled_grid[[x, y]] || " "
+#   end
+#   puts
+# end
+
+def count_dots(grid, visited, x, y)
+  return 0 if visited[[x, y]]
+  visited[[x, y]] = true
+
+  return 0 if grid[[x, y]] == "#"
+
+  neighs = count_dots(grid, visited, x-1, y) +
+           count_dots(grid, visited, x+1, y) +
+           count_dots(grid, visited, x, y-1) +
+           count_dots(grid, visited, x, y+1)
+
+  if grid[[x, y]] == "."
+    neighs + 1
+  else
+    neighs
+  end
+end
+
+part2 = count_dots(upscaled_grid, {}, 200, 200) # TODO: this 200, 200 depends on the input
+puts part2
