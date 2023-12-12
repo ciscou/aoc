1578b9bbff648678a169fac201573cb8cf0fc04b
Francis <francismpp@gmail.com>
2023-12-12 12:52:26 +0100

    2023 12 p2 bruteforcing...

--- 2023/12.rb
+++ 2023/12.rb
@@ -2,12 +2,13 @@ INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 
 rows_and_groups = INPUT.map do |line|
   row, groups = line.split(" ")
+  row = ([row] * 5).join("?")
+  groups = ([groups] * 5).join(",")
   { row: row, groups: groups.split(",").map(&:to_i) }
 end
 
 def possible_arrangements(row, groups, row_offset, groups_offset)
-  if groups_offset == groups.length # TODO: make sure there are no ? in row? make sure row matches original groups?
-    p [row, groups]
+  if groups_offset == groups.length
     if row[row_offset..].include?("#")
       return 0
     else
@@ -24,9 +25,9 @@ def possible_arrangements(row, groups, row_offset, groups_offset)
   # try to skip this question mark
   branch1 = case row[row_offset]
             when "?"
-              row[row_offset] = "."
+              # row[row_offset] = "."
               res = possible_arrangements(row, groups, row_offset + 1, groups_offset)
-              row[row_offset] = "?"
+              # row[row_offset] = "?"
               res
             else
               0
@@ -36,16 +37,16 @@ def possible_arrangements(row, groups, row_offset, groups_offset)
   suffix = row[row_offset + group, 1]
   branch2 = case prefix.gsub("?", "#") + (suffix || "").gsub("?", ".")
             when "#" * group # EOS
-              row[row_offset, group] = "#" * group
+              # row[row_offset, group] = "#" * group
               res = possible_arrangements(row, groups, row_offset + group, groups_offset + 1)
-              row[row_offset, group] = prefix
+              # row[row_offset, group] = prefix
               res
             when "#" * group + "."
-              row[row_offset, group] = "#" * group
-              row[row_offset + group, 1] = "."
+              # row[row_offset, group] = "#" * group
+              # row[row_offset + group, 1] = "."
               res = possible_arrangements(row, groups, row_offset + group + 1, groups_offset + 1)
-              row[row_offset, group] = prefix
-              row[row_offset + group, 1] = suffix
+              # row[row_offset, group] = prefix
+              # row[row_offset + group, 1] = suffix
               res
             else
               0
@@ -55,10 +56,10 @@ def possible_arrangements(row, groups, row_offset, groups_offset)
 end
 
 part1 = 0
-rows_and_groups.each do |row_and_groups|
-  p row_and_groups
+rows_and_groups.each_with_index do |row_and_groups, i|
+  start = Time.now
   kk = possible_arrangements(row_and_groups[:row], row_and_groups[:groups], 0, 0)
-  puts kk
+  p [i+1, kk, Time.now - start]
   part1 += kk
 end
 puts part1
