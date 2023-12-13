585091fc2566882da4623d516e6e5604a5231c8f
Francis <francismpp@gmail.com>
2023-12-13 10:29:25 +0100

    2023 12 p2 memoization

--- 2023/12.rb
+++ 2023/12.rb
@@ -7,11 +7,17 @@ rows_and_groups = INPUT.map do |line|
   { row: row, groups: groups.split(",").map(&:to_i) }
 end
 
-def possible_arrangements(row, groups, row_offset, groups_offset)
+def possible_arrangements(row, groups, row_offset, groups_offset, memo)
+  memok = [row_offset, groups_offset]
+  memod = memo[memok]
+  return memod if memod
+
   if groups_offset == groups.length
     if row[row_offset..].include?("#")
+      memo[memok] = 0
       return 0
     else
+      memo[memok] = 1
       return 1
     end
   end
@@ -26,7 +32,7 @@ def possible_arrangements(row, groups, row_offset, groups_offset)
   branch1 = case row[row_offset]
             when "?"
               # row[row_offset] = "."
-              res = possible_arrangements(row, groups, row_offset + 1, groups_offset)
+              res = possible_arrangements(row, groups, row_offset + 1, groups_offset, memo)
               # row[row_offset] = "?"
               res
             else
@@ -38,13 +44,13 @@ def possible_arrangements(row, groups, row_offset, groups_offset)
   branch2 = case prefix.gsub("?", "#") + (suffix || "").gsub("?", ".")
             when "#" * group # EOS
               # row[row_offset, group] = "#" * group
-              res = possible_arrangements(row, groups, row_offset + group, groups_offset + 1)
+              res = possible_arrangements(row, groups, row_offset + group, groups_offset + 1, memo)
               # row[row_offset, group] = prefix
               res
             when "#" * group + "."
               # row[row_offset, group] = "#" * group
               # row[row_offset + group, 1] = "."
-              res = possible_arrangements(row, groups, row_offset + group + 1, groups_offset + 1)
+              res = possible_arrangements(row, groups, row_offset + group + 1, groups_offset + 1, memo)
               # row[row_offset, group] = prefix
               # row[row_offset + group, 1] = suffix
               res
@@ -52,13 +58,14 @@ def possible_arrangements(row, groups, row_offset, groups_offset)
               0
             end
 
+  memo[memok] = branch1 + branch2
   branch1 + branch2
 end
 
 part1 = 0
 rows_and_groups.each_with_index do |row_and_groups, i|
   start = Time.now
-  kk = possible_arrangements(row_and_groups[:row], row_and_groups[:groups], 0, 0)
+  kk = possible_arrangements(row_and_groups[:row], row_and_groups[:groups], 0, 0, {})
   p [i+1, kk, Time.now - start]
   part1 += kk
 end
