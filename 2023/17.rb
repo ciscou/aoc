eb6e1c5d1a0baf39e034427f51b438bf9a324c6d
Francis <francismpp@gmail.com>
2024-02-06 23:31:13 +0100

    2023 17 p1

---
+++ 2023/17.rb
@@ -0,0 +1,170 @@
+INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
+
+grid = {}
+
+INPUT.each_with_index do |line, row|
+  line.chars.each_with_index do |char, col|
+    grid[[row, col]] = char.to_i
+  end
+end
+
+rows = grid.keys.map(&:first).max + 1
+cols = grid.keys.map(&:last).max + 1
+
+initial_node_1 = {
+  row: 0,
+  col: 1,
+  dir: :right,
+  straight: 1,
+  loss: 0,
+  path: [:right],
+}
+
+initial_node_2 = {
+  row: 1,
+  col: 0,
+  dir: :down,
+  straight: 1,
+  loss: 0,
+  path: [:down],
+}
+
+stack = []
+stack.push(initial_node_1)
+stack.push(initial_node_2)
+
+seen = Hash.new(Float::INFINITY)
+
+part1 = Float::INFINITY
+
+until stack.empty?
+  node = stack.pop
+
+  next if node[:straight] > 3
+  next if node[:row] < 0
+  next if node[:col] < 0
+  next if node[:row] >= rows
+  next if node[:col] >= cols
+
+  dist = rows - node[:row] + cols - node[:col]
+  next if node[:loss] + dist > part1
+
+  if node[:row] == rows - 1 && node[:col] == cols - 1
+    loss = node[:loss] + grid[[node[:row], node[:col]]]
+    # puts "goal: #{loss}"
+    if loss < part1
+      part1 = loss
+      # p part1
+    end
+  end
+
+  next if seen[[node[:row], node[:col], node[:dir], node[:straight]]] <= node[:loss]
+  seen[[node[:row], node[:col], node[:dir], node[:straight]]] = node[:loss]
+
+  case node[:dir]
+  when :up
+    stack.push(
+      row: node[:row] - 1,
+      col: node[:col],
+      dir: node[:dir],
+      straight: node[:straight] + 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:up],
+    )
+    stack.push(
+      row: node[:row],
+      col: node[:col] - 1,
+      dir: :left,
+      straight: 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:left],
+    )
+    stack.push(
+      row: node[:row],
+      col: node[:col] + 1,
+      dir: :right,
+      straight: 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:right],
+    )
+  when :down
+    stack.push(
+      row: node[:row] + 1,
+      col: node[:col],
+      dir: node[:dir],
+      straight: node[:straight] + 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:down],
+    )
+    stack.push(
+      row: node[:row],
+      col: node[:col] - 1,
+      dir: :left,
+      straight: 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:left],
+    )
+    stack.push(
+      row: node[:row],
+      col: node[:col] + 1,
+      dir: :right,
+      straight: 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:right],
+    )
+  when :left
+    stack.push(
+      row: node[:row],
+      col: node[:col] - 1,
+      dir: node[:dir],
+      straight: node[:straight] + 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:left],
+    )
+    stack.push(
+      row: node[:row] - 1,
+      col: node[:col],
+      dir: :up,
+      straight: 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:up],
+    )
+    stack.push(
+      row: node[:row] + 1,
+      col: node[:col],
+      dir: :down,
+      straight: 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:down],
+    )
+  when :right
+    stack.push(
+      row: node[:row],
+      col: node[:col] + 1,
+      dir: node[:dir],
+      straight: node[:straight] + 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:right],
+    )
+    stack.push(
+      row: node[:row] - 1,
+      col: node[:col],
+      dir: :up,
+      straight: 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:up],
+    )
+    stack.push(
+      row: node[:row] + 1,
+      col: node[:col],
+      dir: :down,
+      straight: 1,
+      loss: node[:loss] + grid[[node[:row], node[:col]]],
+      path: node[:path] + [:down],
+    )
+  else
+    raise "invalid dir #{dir.inspect}"
+  end
+end
+
+puts part1
