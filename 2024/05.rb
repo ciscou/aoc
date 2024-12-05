c884fdd2e205b76d74dee6675714b7d61e01bba5
Francis <francismpp@gmail.com>
2024-12-05 10:27:03 +0100

    2024 05 p2

--- 2024/05.rb
+++ 2024/05.rb
@@ -2,14 +2,16 @@ INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 
 rules_lines, updates_lines = INPUT.chunk { _1.empty? && nil }.map(&:last)
 
-rules = rules_lines.map { _1.split("|").map(&:to_i) }.group_by(&:first)
+rules = rules_lines.map { _1.split("|").map(&:to_i) }
 updates = updates_lines.map { _1.split(",").map(&:to_i) }
 
-valid_updates = updates.select do |update|
+rules_by_first = rules.group_by(&:first)
+
+valid_updates, invalid_updates = updates.partition do |update|
   valid = true
   seen = Set.new
   update.each do |n|
-    if rules[n] && rules[n].any? { |_, m| seen.include? m  }
+    if (rules_by_first[n] || []).any? { |_, m| seen.include? m  }
       valid = false
     end
     seen.add(n)
@@ -19,3 +21,28 @@ end
 
 part1 = valid_updates.sum { _1[_1.length / 2] }
 puts part1
+
+def fix_ordering(update, rules)
+  fixed = []
+  need = update.to_set
+
+  loop do
+    break if need.empty?
+
+    ns = need.select do |m|
+      (rules[m] || []).map(&:first).none? { need.include?(_1) }
+    end
+    raise "expected 1 value but got #{ns.inspect}" unless ns.length == 1
+    n = ns.first
+
+    fixed << n
+    need.delete(n)
+  end
+
+  fixed
+end
+
+rules_by_last = rules.group_by(&:last)
+
+part2 = invalid_updates.map { fix_ordering(_1, rules_by_last) }.sum { _1[_1.length / 2] }
+puts part2
