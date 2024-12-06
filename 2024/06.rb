03f9d7472d7a2a9bf5dd1665df23effd97a07574
Francis <francismpp@gmail.com>
2024-12-06 13:28:49 +0100

    2024 06 p2

--- 2024/06.rb
+++ 2024/06.rb
@@ -55,3 +55,59 @@ loop do
 end
 
 puts visited.size
+
+part2 = 0
+
+map.length.times do |obstacle_row|
+  map[obstacle_row].length.times do |obstacle_col|
+    map = INPUT.map(&:chars)
+
+    guard_row = nil
+    guard_col = nil
+    guard_dir = nil
+
+    map.length.times do |row|
+      map[row].length.times do |col|
+        if map[row][col] == "^"
+          map[row][col] = "."
+          guard_row = row
+          guard_col = col
+          guard_dir = :up
+        end
+      end
+    end
+
+    next if map[obstacle_row][obstacle_col] == "#"
+    next if guard_row == obstacle_row && guard_col == obstacle_col
+
+    map[obstacle_row][obstacle_col] = "#"
+
+    visited = Set.new
+    visited.add([guard_row, guard_col, guard_dir])
+
+    loop do
+      drow, dcol = DROW_DCOL[guard_dir]
+
+      guard_row += drow
+      guard_col += dcol
+
+      break if guard_row < 0
+      break if guard_col < 0
+      break unless guard_row < map.length
+      break unless guard_col < map[guard_row].length
+
+      if map[guard_row][guard_col] == "#"
+        guard_row -= drow
+        guard_col -= dcol
+        guard_dir = TURN_RIGHT[guard_dir]
+      else
+        unless visited.add?([guard_row, guard_col, guard_dir])
+          part2 += 1
+          break
+        end
+      end
+    end
+  end
+end
+
+puts part2
