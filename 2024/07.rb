4cd9a5e82e281b13b706e528060e2dd40be1d66e
Francis <francismpp@gmail.com>
2024-12-07 12:31:15 +0100

    2024 07 p1

---
+++ 2024/07.rb
@@ -0,0 +1,33 @@
+INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
+
+equations = INPUT.map do |line|
+  a, b = line.split(": ")
+  [a.to_i, b.split(" ").map(&:to_i)]
+end
+
+def has_solution?(solution, numbers)
+  q = []
+  q << numbers
+  until q.empty?
+    ns = q.pop
+    a, b, *rest = ns
+    if b.nil?
+      return true if a == solution
+    else
+      q << [a + b, *rest]
+      q << [a * b, *rest]
+    end
+  end
+  false
+end
+
+part1 = equations.sum do |solution, numbers|
+  p [solution, numbers]
+  if has_solution?(solution, numbers)
+    solution
+  else
+    0
+  end
+end
+
+puts part1
