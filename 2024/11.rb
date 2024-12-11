f3a8baf4beec991fba6addf6982d842123656b25
Francis <francismpp@gmail.com>
2024-12-11 11:05:41 +0100

    2024 11 p2

--- 2024/11.rb
+++ 2024/11.rb
@@ -1,24 +1,25 @@
 INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 
-stones = INPUT.first.split(" ").map { [_1.to_i] }
+stones = Hash.new(0)
+INPUT.first.split(" ").map(&:to_i).each { stones[_1] += 1 }
 
-25.times do
-  stones.map! do |stone_group|
-    stone_group.map do |stone|
-      if stone == 0
-        [1]
+75.times do
+  next_stones = stones.dup
+  stones.each do |k, v|
+    next_stones[k] -= v
+    if k == 0
+      next_stones[1] += v
+    else
+      to_s = k.to_s
+      p, q = to_s.length.divmod(2)
+      if q == 0
+        next_stones[to_s[0, p].to_i] += v
+        next_stones[to_s[p, p].to_i] += v
       else
-        to_s = stone.to_s
-        p, q = to_s.length.divmod(2)
-        if q == 0
-          [to_s[0, p].to_i, to_s[p, p].to_i]
-        else
-          [stone * 2024]
-        end
+        next_stones[k * 2024] += v
       end
-    end.flatten(1)
+    end
   end
+  stones = next_stones
+  puts stones.values.sum if [24, 74].include?(_1)
 end
-
-part1 = stones.sum(&:length)
-puts part1
