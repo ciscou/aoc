f509ede7a7efc80f08e7a2f967ca17a8afd8952c
Francis <francismpp@gmail.com>
2024-12-12 19:30:01 +0100

    refactor

--- 2024/12.rb
+++ 2024/12.rb
@@ -3,20 +3,26 @@ INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 grid = INPUT.map(&:chars)
 
 def dfs(grid, garden, gardens_by_pos, r, c)
+  return true if gardens_by_pos[[r, c]] == garden
+
   return if r < 0
   return if c < 0
   return unless r < grid.length
   return unless c < grid[r].length
+
   return unless grid[r][c] == garden[:letter]
+
   return if gardens_by_pos.key?([r, c])
 
   gardens_by_pos[[r, c]] = garden
   garden[:area] += 1
 
-  dfs(grid, garden, gardens_by_pos, r-1, c)
-  dfs(grid, garden, gardens_by_pos, r+1, c)
-  dfs(grid, garden, gardens_by_pos, r, c-1)
-  dfs(grid, garden, gardens_by_pos, r, c+1)
+  garden[:perimeter] += 1 unless dfs(grid, garden, gardens_by_pos, r-1, c)
+  garden[:perimeter] += 1 unless dfs(grid, garden, gardens_by_pos, r+1, c)
+  garden[:perimeter] += 1 unless dfs(grid, garden, gardens_by_pos, r, c-1)
+  garden[:perimeter] += 1 unless dfs(grid, garden, gardens_by_pos, r, c+1)
+
+  true
 end
 
 gardens_by_pos = {}
@@ -30,32 +36,6 @@ grid.each_with_index do |row, r|
 
     dfs(grid, gardens.last, gardens_by_pos, r, c)
   end
-end
-
-grid.length.times do |r|
-  last_garden = nil
-  grid[r].length.times do |c|
-    garden = gardens_by_pos[[r, c]]
-    if last_garden.nil? || last_garden[:letter] != garden[:letter]
-      last_garden[:perimeter] += 1 unless last_garden.nil?
-      garden[:perimeter] += 1
-      last_garden = garden
-    end
-  end
-  last_garden[:perimeter] += 1
-end
-
-grid.first.length.times do |c|
-  last_garden = nil
-  grid.length.times do |r|
-    garden = gardens_by_pos[[r, c]]
-    if last_garden.nil? || last_garden[:letter] != garden[:letter]
-      last_garden[:perimeter] += 1 unless last_garden.nil?
-      garden[:perimeter] += 1
-      last_garden = garden
-    end
-  end
-  last_garden[:perimeter] += 1
 end
 
 part1 = gardens.sum { _1[:area] * _1[:perimeter] }
