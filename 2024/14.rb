bc92591bd60212417eeabe1899cac7690d654cfd
Francis <francismpp@gmail.com>
2024-12-14 09:11:26 +0100

    2024 14 p2

--- 2024/14.rb
+++ 2024/14.rb
@@ -16,21 +16,47 @@ end
 w = 101
 h = 103
 
-robots.each do |r|
-  r[:x] = (r[:x] + r[:vx] * 100) % w
-  r[:y] = (r[:y] + r[:vy] * 100) % h
+def display_robots(robots, w, h)
+  grid = h.times.map { w.times.map { " " } }
+  robots.each { grid[_1[:y]][_1[:x]] = "#" }
+  if grid.any? { |l| l.join.include?("#####################") }
+    puts grid.map(&:join)
+    true
+  else
+    false
+  end
 end
 
-qs = { tl: 0, tr: 0, bl: 0, br: 0 }
+elapsed = 0
+loop do
+  robots.each do |r|
+    r[:x] = (r[:x] + r[:vx] * 1) % w
+    r[:y] = (r[:y] + r[:vy] * 1) % h
+  end
 
-robots.each do |r|
-  q = nil
-  q = :tl if r[:x] < w/2 && r[:y] < h/2
-  q = :tr if r[:x] > w/2 && r[:y] < h/2
-  q = :bl if r[:x] < w/2 && r[:y] > h/2
-  q = :br if r[:x] > w/2 && r[:y] > h/2
-  qs[q] += 1 if q
+  qs = { tl: 0, tr: 0, bl: 0, br: 0 }
+
+  robots.each do |r|
+    q = nil
+    q = :tl if r[:x] < w/2 && r[:y] < h/2
+    q = :tr if r[:x] > w/2 && r[:y] < h/2
+    q = :bl if r[:x] < w/2 && r[:y] > h/2
+    q = :br if r[:x] > w/2 && r[:y] > h/2
+    qs[q] += 1 if q
+  end
+
+  elapsed += 1
+
+  # if qs[:tl] == qs[:tr] && qs[:bl] == qs[:br]
+    if display_robots(robots, w, h)
+      puts elapsed
+      exit
+    end
+  # end
+
+  puts elapsed if elapsed % 1_000_000 == 0
+
+  if elapsed == 100
+    puts qs.values.reduce(:*)
+  end
 end
-
-part1 = qs.values.reduce(:*)
-puts part1
