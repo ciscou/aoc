8ed40bc8d9464cb4dce3ff2b15a6faa157f992a9
Francis <francismpp@gmail.com>
2024-12-16 22:11:17 +0100

    proper dijstra using priority queue

--- 2024/16.rb
+++ 2024/16.rb
@@ -1,3 +1,58 @@
+# stolen from https://gist.github.com/brianstorti/e20300eb2e7d62b87849
+class PriorityQueue
+  attr_reader :elements
+
+  def initialize
+    @elements = [nil]
+  end
+
+  def <<(element)
+    @elements << element
+    bubble_up(@elements.size - 1)
+  end
+
+  alias_method :push, :<<
+
+  def pop
+    exchange(1, @elements.size - 1)
+    max = @elements.pop
+    bubble_down(1)
+    max
+  end
+
+  private
+
+  def bubble_up(index)
+    parent_index = (index / 2)
+
+    return if index <= 1
+    return if @elements[parent_index][:priority] >= @elements[index][:priority]
+
+    exchange(index, parent_index)
+    bubble_up(parent_index)
+  end
+
+  def bubble_down(index)
+    child_index = (index * 2)
+
+    return if child_index > @elements.size - 1
+
+    not_the_last_element = child_index < @elements.size - 1
+    left_element = @elements[child_index]
+    right_element = @elements[child_index + 1]
+    child_index += 1 if not_the_last_element && right_element[:priority] > left_element[:priority]
+
+    return if @elements[index][:priority] >= @elements[child_index][:priority]
+
+    exchange(index, child_index)
+    bubble_down(child_index)
+  end
+
+  def exchange(source, target)
+    @elements[source], @elements[target] = @elements[target], @elements[source]
+  end
+end
+
 INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 
 GRID = INPUT.map(&:chars)
@@ -19,55 +74,57 @@ H.times do |r|
   end
 end
 
-def dijstra(row, col, dr, dc, visited, cost, path, h)
-  return Float::INFINITY if row < 0
-  return Float::INFINITY if col < 0
-  return Float::INFINITY unless row < H
-  return Float::INFINITY unless col < W
+pq = PriorityQueue.new
 
-  return Float::INFINITY if GRID[row][col] == "#"
+pq.push(state: [reindeer_row, reindeer_col, reindeer_dr, reindeer_dc, [[reindeer_row, reindeer_col]]], priority: 0)
 
-  if GRID[row][col] == "E"
-    h[cost] ||= []
-    h[cost] << path
-    return cost
+visited = {}
+
+best = nil
+paths = []
+
+loop do
+  node = pq.pop
+  break if node.nil?
+
+  state, priority = node.values_at(:state, :priority)
+
+  cost = -priority
+
+  r, c, dr, dc, path = state
+
+  next if r < 0
+  next if c < 0
+  next unless r < H
+  next unless c < W
+
+  next if GRID[r][c] == "#"
+
+  if GRID[r][c] == "E"
+    best = cost if best.nil?
+    break if cost > best
+
+    paths << path
   end
 
-  # poor man's dijstra, if only there were a priority queue in ruby stdlib...
-  return Float::INFINITY if visited[[row, col, dr, dc]] < cost
-  visited[[row, col, dr, dc]] = cost
+  next if visited[[r, c, dr, dc]] && visited[[r, c, dr, dc]] < cost
+  visited[[r, c, dr, dc]] = cost
 
-  best = Float::INFINITY
+  pq.push(state: [r + dr, c + dc, dr, dc, path + [[r + dr, c + dc]]], priority: -(cost + 1))
 
-  best = [best, dijstra(row + dr, col + dc, dr, dc, visited, cost + 1, path + [[row + dr, col + dc]], h)].min
-
-  case [dr, dc]
-  when [0, 1]
-    best = [best, dijstra(row, col, 1, 0, visited, cost + 1000, path, h)].min
-    best = [best, dijstra(row, col, -1, 0, visited, cost + 1000, path, h)].min
-  when [0, -1]
-    best = [best, dijstra(row, col, 1, 0, visited, cost + 1000, path, h)].min
-    best = [best, dijstra(row, col, -1, 0, visited, cost + 1000, path, h)].min
-  when [1, 0]
-    best = [best, dijstra(row, col, 0, 1, visited, cost + 1000, path, h)].min
-    best = [best, dijstra(row, col, 0, -1, visited, cost + 1000, path, h)].min
-  when [-1, 0]
-    best = [best, dijstra(row, col, 0, 1, visited, cost + 1000, path, h)].min
-    best = [best, dijstra(row, col, 0, -1, visited, cost + 1000, path, h)].min
+  if dr == 0
+    pq.push(state: [r, c, 1, 0, path], priority: -(cost + 1000))
+    pq.push(state: [r, c, -1, 0, path], priority: -(cost + 1000))
   else
-    raise
+    pq.push(state: [r, c, 0, 1, path], priority: -(cost + 1000))
+    pq.push(state: [r, c, 0, -1, path], priority: -(cost + 1000))
   end
-
-  best
 end
 
-visited = Hash.new(Float::INFINITY)
-h = {}
-part1 = dijstra(reindeer_row, reindeer_col, reindeer_dr, reindeer_dc, visited, 0, [[reindeer_row, reindeer_col]], h)
-puts part1
+puts best
+
 set = Set.new
-h[part1].each do |path|
+paths.each do |path|
   path.each { set.add _1 }
 end
-part2 = set.size
-puts part2
+puts set.size
