1b60b897b1ac850753174cac309579b9d2fd6571
Francis <francismpp@gmail.com>
2024-12-17 23:30:47 +0100

    refactor

--- 2024/17.rb
+++ 2024/17.rb
@@ -1,22 +1,6 @@
 INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 
-def combo(literal, a, b, c)
-  case literal
-  when 0..3
-    literal
-  when 4
-    a
-  when 5
-    b
-  when 6
-    c
-  end
-end
-
-a = INPUT[0].split(": ").last.to_i
-b = INPUT[1].split(": ").last.to_i
-c = INPUT[2].split(": ").last.to_i
-
+a, b, c = 3.times.map { INPUT[_1].split(": ").last.to_i }
 program = INPUT[4].split(": ").last.split(",").map(&:to_i)
 
 def execute(program, a, b, c)
@@ -27,19 +11,22 @@ def execute(program, a, b, c)
   loop do
     break unless pc < program.length
 
+    instruction = program[pc]
     literal = program[pc + 1]
-    combo = combo(literal, a, b, c)
+    pc += 2
 
-    case program[pc]
+    combo = [literal, literal, literal, literal, a, b, c].at(literal)
+
+    case instruction
     when 0 # adv
       a = a / (2 ** combo)
     when 1 # bxl
       b = b ^ literal
     when 2 # bst
-      b = combo(literal, a, b, c) % 8
+      b = combo % 8
     when 3 # jnz
       unless a == 0
-        pc = literal - 2
+        pc = literal
       end
     when 4 # bxc
       b = b ^ c
@@ -50,8 +37,6 @@ def execute(program, a, b, c)
     when 7 # cdv
       c = a / (2 ** combo)
     end
-
-    pc += 2
   end
 
   out
@@ -60,12 +45,11 @@ end
 part1 = execute(program, a, b, c).join(",")
 puts part1
 
-a = 1
-
-(1..program.length).each do |l|
-  a += 1 until execute(program, a, b, c).last(l) == program.last(l)
+a = 0
+program.length.times do |l|
   a *= 8
+  a += 1 until execute(program, a, b, c) == program.last(l + 1)
 end
 
-part2 = a / 8
+part2 = a
 puts part2
