de1c29f8fda0740a84acb8240fff18e6001d8ffa
Francis <francismpp@gmail.com>
2024-12-20 18:25:50 +0100

    minirefactor

--- 2024/20.rb
+++ 2024/20.rb
@@ -1,31 +1,11 @@
 INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 
-GRID = INPUT.map { _1.chars }
+GRID = INPUT.map(&:chars)
 H = GRID.length
 W = GRID.first.length
 
-start_row = nil
-start_col = nil
-end_row = nil
-end_col = nil
-
-H.times do |r|
-  W.times do |c|
-    if GRID[r][c] == "S"
-      start_row = r
-      start_col = c
-      GRID[r][c] = "."
-    end
-    if GRID[r][c] == "E"
-      end_row = r
-      end_col = c
-      GRID[r][c] = "."
-    end
-  end
-end
-
-DISTANCE_FROM_START = H.times.map { W.times.map { nil } }
-DISTANCE_TO_END = H.times.map { W.times.map { nil } }
+DISTANCE_FROM_START = Array.new(H) { Array.new(W, nil) }
+DISTANCE_TO_END = Array.new(H) { Array.new(W, nil) }
 
 def bfs(start_row, start_col, h)
   q = []
@@ -50,8 +30,6 @@ def bfs(start_row, start_col, h)
     q << [row, col - 1, steps + 1]
     q << [row, col + 1, steps + 1]
   end
-
-  Float::INFINITY
 end
 
 def cheats_from(sr, sc, limit)
@@ -73,11 +51,8 @@ def cheats_from(sr, sc, limit)
   res
 end
 
-bfs(start_row, start_col, DISTANCE_FROM_START)
-bfs(end_row, end_col, DISTANCE_TO_END)
-
-def saves(limit, no_cheats)
-  saves = []
+def helper(limit)
+  res = []
 
   H.times.each do |sr|
     W.times.each do |sc|
@@ -88,24 +63,41 @@ def saves(limit, no_cheats)
         two = DISTANCE_TO_END[er][ec]
         next unless two
 
-        saves << no_cheats - (one + two + dist)
+        res << one + two + dist
       end
     end
   end
 
-  saves
+  res
 end
+
+start_row = nil
+start_col = nil
+end_row = nil
+end_col = nil
+
+H.times do |r|
+  W.times do |c|
+    if GRID[r][c] == "S"
+      start_row = r
+      start_col = c
+      GRID[r][c] = "."
+    end
+    if GRID[r][c] == "E"
+      end_row = r
+      end_col = c
+      GRID[r][c] = "."
+    end
+  end
+end
+
+bfs(start_row, start_col, DISTANCE_FROM_START)
+bfs(end_row, end_col, DISTANCE_TO_END)
 
 no_cheats = DISTANCE_TO_END[start_row][start_col]
 
-part1 = 0
-saves(2, no_cheats).each do |save|
-  part1 += 1 if save >= 100
-end
-puts part1
+part1 = helper(2).count { no_cheats - _1 >= 100 }
+part2 = helper(20).count { no_cheats - _1 >= 100 }
 
-part2 = 0
-saves(20, no_cheats).each do |save|
-  part2 += 1 if save >= 100
-end
+puts part1
 puts part2
