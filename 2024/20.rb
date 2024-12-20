59056714a86c3523f7ed09bf382c6c0c35fa77af
Francis <francismpp@gmail.com>
2024-12-20 12:12:53 +0100

    2024 20 p2 meta cheat

--- 2024/20.rb
+++ 2024/20.rb
@@ -1,8 +1,8 @@
 INPUT = File.readlines(__FILE__.sub('.rb', '.txt'), chomp: true)
 
-grid = INPUT.map { _1.chars }
-H = grid.length
-W = grid.first.length
+GRID = INPUT.map { _1.chars }
+H = GRID.length
+W = GRID.first.length
 
 start_row = nil
 start_col = nil
@@ -11,24 +11,25 @@ end_col = nil
 
 H.times do |r|
   W.times do |c|
-    if grid[r][c] == "S"
+    if GRID[r][c] == "S"
       start_row = r
       start_col = c
-      grid[r][c] = "."
+      GRID[r][c] = "."
     end
-    if grid[r][c] == "E"
+    if GRID[r][c] == "E"
       end_row = r
       end_col = c
-      grid[r][c] = "."
+      GRID[r][c] = "."
     end
   end
 end
 
-def bfs(grid, start_row, start_col, end_row, end_col, cheat_start_row, cheat_start_col)
+DISTANCE_FROM_START = H.times.map { W.times.map { nil } }
+DISTANCE_TO_END = H.times.map { W.times.map { nil } }
+
+def bfs(start_row, start_col, h)
   q = []
   q << [start_row, start_col, 0]
-
-  v = Set.new
 
   until q.empty?
     n = q.shift
@@ -39,17 +40,10 @@ def bfs(grid, start_row, start_col, end_row, end_col, cheat_start_row, cheat_sta
     next unless row < H
     next unless col < W
 
-    cheat = row == cheat_start_row && col == cheat_start_col
+    next if GRID[row][col] == "#"
 
-    if cheat
-    else
-      next if grid[row][col] == "#"
-    end
-
-    next if v.include?([row, col])
-    v.add([row, col])
-
-    return steps if row == end_row && col == end_col
+    next if h[row][col]
+    h[row][col] = steps
 
     q << [row - 1, col, steps + 1]
     q << [row + 1, col, steps + 1]
@@ -57,22 +51,47 @@ def bfs(grid, start_row, start_col, end_row, end_col, cheat_start_row, cheat_sta
     q << [row, col + 1, steps + 1]
   end
 
-  raise "impossible"
+  Float::INFINITY
 end
 
-no_cheats = bfs(grid, start_row, start_col, end_row, end_col, nil, nil)
+bfs(start_row, start_col, DISTANCE_FROM_START)
+bfs(end_row, end_col, DISTANCE_TO_END)
 
-saves = []
+def saves(limit, no_cheats)
+  saves = []
 
-H.times.each do |r|
-  W.times.each do |c|
-    saves << no_cheats - bfs(grid, start_row, start_col, end_row, end_col, r, c)
-    p saves
+  H.times.each do |sr|
+    W.times.each do |sc|
+      one = DISTANCE_FROM_START[sr][sc]
+      next unless one
+
+      H.times.each do |er|
+        W.times.each do |ec|
+          two = DISTANCE_TO_END[er][ec]
+          next unless two
+
+          cheat = (er - sr).abs + (ec - sc).abs
+          next unless cheat <= limit
+
+          saves << no_cheats - (one + two + cheat)
+        end
+      end
+    end
   end
+
+  saves
 end
+
+no_cheats = DISTANCE_TO_END[start_row][start_col]
 
 part1 = 0
-saves.each do |save|
+saves(2, no_cheats).each do |save|
   part1 += 1 if save >= 100
 end
 puts part1
+
+part2 = 0
+saves(20, no_cheats).each do |save|
+  part2 += 1 if save >= 100
+end
+puts part2
